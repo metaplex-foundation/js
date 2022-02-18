20530dbad6895e34adfd5e8119b69cac1fc7bc49
Loris Leiva <loris.leiva@gmail.com>
2022-02-18 13:46:42 +0000

    Update TransactionBuilder.ts

--- src/utils/TransactionBuilder.ts
+++ src/utils/TransactionBuilder.ts
@@ -7,22 +7,69 @@ export interface TransactionBuilderRecord {
 }
 
 export class TransactionBuilder {
+
+  /** The list of all instructions and their respective signers. */
   private records: TransactionBuilderRecord[] = [];
 
-  prepend(instruction: TransactionInstruction, signers: Signer[] = [], key?: string) {
+  /** Options used when building the transaction. */
+  private transactionOptions: TransactionCtorFields;
+
+  constructor(transactionOptions: TransactionCtorFields = {}) {
+    this.transactionOptions = transactionOptions;
+  }
+
+  prepend(instruction: TransactionInstruction, signers: Signer[] = [], key?: string): TransactionBuilder {
     this.records.unshift({ key, instruction, signers });
 
     return this;
   }
 
-  append(instruction: TransactionInstruction, signers: Signer[] = [], key?: string) {
+  append(instruction: TransactionInstruction, signers: Signer[] = [], key?: string): TransactionBuilder {
     this.records.push({ key, instruction, signers });
 
     return this;
   }
 
-  add(instruction: TransactionInstruction, signers: Signer[] = [], key?: string) {
+  add(instruction: TransactionInstruction, signers: Signer[] = [], key?: string): TransactionBuilder {
     return this.append(instruction, signers, key);
+  }
+
+  addRecords(...records: TransactionBuilderRecord[]): TransactionBuilder {
+    return records.reduce((builder: TransactionBuilder, { instruction, signers, key }) => {
+      return builder.add(instruction, signers, key);
+    }, this)
+  }
+
+  merge(that: TransactionBuilder): TransactionBuilder {
+    return this.addRecords(...that.getRecords());
+  }
+
+  splitUsingKey(key: string, include: boolean = true): [TransactionBuilder, TransactionBuilder] {
+    const firstBuilder = new TransactionBuilder(this.transactionOptions);
+    const secondBuilder = new TransactionBuilder(this.transactionOptions);
+    let keyPosition = this.records.findIndex(record => record.key === key)
+
+    if (keyPosition > -1) {
+      keyPosition += include ? 1 : 0; 
+      firstBuilder.addRecords(...this.records.slice(0, keyPosition))
+      firstBuilder.addRecords(...this.records.slice(keyPosition))
+    } else {
+      firstBuilder.merge(this)
+    }
+
+    return [firstBuilder, secondBuilder];
+  }
+
+  splitBeforeKey(key: string): [TransactionBuilder, TransactionBuilder] {
+    return this.splitUsingKey(key, false);
+  }
+
+  splitAfterKey(key: string): [TransactionBuilder, TransactionBuilder] {
+    return this.splitUsingKey(key, true);
+  }
+
+  getRecords(): TransactionBuilderRecord[] {
+    return this.records;
   }
 
   getInstructions(): TransactionInstruction[] {
@@ -40,15 +87,25 @@ export class TransactionBuilder {
       }, []);
   }
 
-  toTransaction(options: TransactionCtorFields): Transaction {
-    const tx = new Transaction(options);
+  setTransactionOptions(transactionOptions: TransactionCtorFields): TransactionBuilder {
+    this.transactionOptions = transactionOptions;
+
+    return this;
+  }
+
+  getTransactionOptions() {
+    return this.transactionOptions;
+  }
+
+  toTransaction(): Transaction {
+    const tx = new Transaction(this.getTransactionOptions());
     tx.add(...this.getInstructions());
 
     return tx;
   }
 
-  async sendTransaction(connection: Connection, txOptions: TransactionCtorFields = {}, sendOptions?: SendOptions): Promise<string> {
-    const tx = this.toTransaction(txOptions);
+  async sendTransaction(connection: Connection, sendOptions: SendOptions = {}): Promise<string> {
+    const tx = this.toTransaction();
 
     return connection.sendTransaction(tx, this.getSigners(), sendOptions)
   }
