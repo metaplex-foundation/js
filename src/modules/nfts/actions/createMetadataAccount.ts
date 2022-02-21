48a5315ab285047985a3cc34531d66905c489140
Loris Leiva <loris.leiva@gmail.com>
2022-02-21 17:23:26 +0000

    Add generated API files for now

--- src/modules/nfts/actions/createMetadataAccount.ts
+++ src/modules/nfts/actions/createMetadataAccount.ts
@@ -1,14 +1,16 @@
 import { PublicKey } from '@solana/web3.js';
 import { Metaplex } from '@/Metaplex';
-import { MetadataAccount, MetadataDataV2Type } from '@/modules/nfts';
+import { MetadataAccount } from '@/modules/nfts';
 import { Pda } from '@/utils';
+import { createCreateMetadataAccountV2Instruction, DataV2 } from '../generated';
 
 export interface CreateMetadataAccountParams {
-  data: MetadataDataV2Type,
+  data: DataV2,
+  isMutable?: boolean,
   mint: PublicKey,
   mintAuthority: PublicKey,
   updateAuthority?: PublicKey,
-  feePayer: PublicKey,
+  feePayer: PublicKey, // TODO: Make optional and defaults to transaction fee payer.
 }
 
 export interface CreateMetadataAccountResult {
@@ -18,5 +20,19 @@ export interface CreateMetadataAccountResult {
 
 export const createMetadataAccount = async (metaplex: Metaplex, params: CreateMetadataAccountParams): Promise<CreateMetadataAccountResult> => {
   const metadata = await MetadataAccount.pda(params.mint);
+
+  const ix = createCreateMetadataAccountV2Instruction({
+    metadata,
+    mint: params.mint,
+    mintAuthority: params.mintAuthority,
+    payer: params.feePayer,
+    updateAuthority: params.updateAuthority ?? params.mintAuthority,
+  }, {
+    createMetadataAccountArgsV2: {
+      data: params.data,
+      isMutable: params.isMutable ?? false,
+    }
+  });
+
   return { metadata, transactionSignature: 'todo' };
 };
