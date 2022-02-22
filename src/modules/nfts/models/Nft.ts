1de07448e4e5cb64c8a00554f9dd1cf9e137340d
Loris Leiva <loris.leiva@gmail.com>
2022-02-22 18:41:37 +0000

    wip

--- src/modules/nfts/models/Nft.ts
+++ src/modules/nfts/models/Nft.ts
@@ -1,15 +1,14 @@
-import { Model, Account } from "@/modules/shared";
-import { MasterEditionV1, MasterEditionV2, Metadata } from "../generated";
+import { Model, MetadataAccount, MasterEditionAccount } from "@/modules/shared";
 
 export class Nft extends Model {
 
   /** The Metadata PDA account defining the NFT. */
-  public readonly metadata: Account<Metadata>;
+  public readonly metadata: MetadataAccount;
 
   /** The optional Metadata Edition PDA account associated with the NFT. */
-  public readonly masterEdition: Account<MasterEditionV1 | MasterEditionV2> | null;
+  public readonly masterEdition: MasterEditionAccount | null;
 
-  constructor(metadata: Account<Metadata>, masterEdition: Account<MasterEditionV1 | MasterEditionV2> | null = null) {
+  constructor(metadata: MetadataAccount, masterEdition: MasterEditionAccount | null = null) {
     super();
     this.metadata = metadata;
     this.masterEdition = masterEdition;
