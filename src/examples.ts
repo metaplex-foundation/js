1c4acc99b2e2196e83d62cb96a622eb81be2cb47
Loris Leiva <loris.leiva@gmail.com>
2022-02-24 18:10:58 +0000

    wip

---
+++ src/examples.ts
@@ -0,0 +1,18 @@
+// Imports
+
+// Initial setup.
+// TODO: Metaplex + NftClient.
+
+// [WRITE] Create a new NFT.
+// TODO
+
+// [WRITE] Update an NFT's metadata.
+// TODO
+
+// [READ] Find an NFT.
+// TODO
+
+// [READ] Find multiple NFTs using a GpaBuilder.
+// TODO
+
+// Don't forget about the thirst category: [REALTIME].
--- src/modules/nfts/models/Nft.ts
+++ src/modules/nfts/models/Nft.ts
@@ -1,17 +1,56 @@
+import { PublicKey } from "@solana/web3.js";
 import { Model } from "@/modules/shared";
 import { MetadataAccount, MasterEditionAccount } from "@/programs/tokenMetadata";
+import { TokenStandard, Collection, Uses, Creator } from "@/programs/tokenMetadata/generated";
+import { bignum } from "@metaplex-foundation/beet";
 
 export class Nft extends Model {
 
   /** The Metadata PDA account defining the NFT. */
-  public readonly metadata: MetadataAccount;
+  public readonly metadataAccount: MetadataAccount;
 
   /** The optional Metadata Edition PDA account associated with the NFT. */
-  public readonly masterEdition: MasterEditionAccount | null;
+  public readonly masterEditionAccount: MasterEditionAccount | null;
 
-  constructor(metadata: MetadataAccount, masterEdition: MasterEditionAccount | null = null) {
+  /** Data from the Metadata account. */
+  public readonly updateAuthority: PublicKey;
+  public readonly mint: PublicKey;
+  public readonly name: string;
+  public readonly symbol: string;
+  public readonly uri: string;
+  public readonly sellerFeeBasisPoints: number;
+  public readonly creators: Creator[] | null;
+  public readonly primarySaleHappened: boolean;
+  public readonly isMutable: boolean;
+  public readonly editionNonce: number | null;
+  public readonly tokenStandard: TokenStandard | null;
+  public readonly collection: Collection | null;
+  public readonly uses: Uses | null;
+
+  /** Data from the MasterEdition account. */
+  public readonly supply: bignum | null;
+  public readonly maxSupply: bignum | null;
+
+  constructor(metadataAccount: MetadataAccount, masterEditionAccount: MasterEditionAccount | null = null) {
     super();
-    this.metadata = metadata;
-    this.masterEdition = masterEdition;
+    this.metadataAccount = metadataAccount;
+    this.masterEditionAccount = masterEditionAccount;
+
+    this.updateAuthority = metadataAccount.data.updateAuthority;
+    this.mint = metadataAccount.data.mint;
+    this.name = metadataAccount.data.data.name;
+    this.symbol = metadataAccount.data.data.symbol;
+    this.uri = metadataAccount.data.data.uri;
+    this.sellerFeeBasisPoints = metadataAccount.data.data.sellerFeeBasisPoints;
+    this.creators = metadataAccount.data.data.creators;
+    this.primarySaleHappened = metadataAccount.data.primarySaleHappened;
+    this.isMutable = metadataAccount.data.isMutable;
+    this.editionNonce = metadataAccount.data.editionNonce;
+    this.tokenStandard = metadataAccount.data.tokenStandard;
+    this.collection = metadataAccount.data.collection;
+    this.uses = metadataAccount.data.uses;
+
+    this.supply = masterEditionAccount?.data.supply ?? null;
+    this.maxSupply = masterEditionAccount?.data.maxSupply ?? null;
   }
 }
