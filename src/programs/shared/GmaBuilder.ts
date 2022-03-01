c1797dafd482e346600ed4a5acb25cbe7c1d278c
Loris Leiva <loris.leiva@gmail.com>
2022-03-01 16:52:15 +0000

    wip

--- src/programs/shared/GmaBuilder.ts
+++ src/programs/shared/GmaBuilder.ts
@@ -38,34 +38,42 @@ export class GmaBuilder {
   }
 
   async getFirst(n?: number): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
-    return [];
+    const end = this.boundNumber(n ?? 1);
+
+    return this.getChunks(this.publicKeys.slice(0, end));
   }
 
   async getLast(n?: number): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
-    return [];
+    const start = this.boundNumber(n ?? 1);
+
+    return this.getChunks(this.publicKeys.slice(- start));
   }
 
   async getBetween(start: number, end: number): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
-    return [];
+    start = this.boundNumber(start);
+    end = this.boundNumber(end);
+    [start, end] = start > end ? [end, start] : [start, end];
+
+    return this.getChunks(this.publicKeys.slice(start, end));
   }
 
   async getPage(page: number, perPage: number): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
-    return [];
+    return this.getBetween((page - 1) * perPage, page * perPage);
   }
 
   async get(): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
-    return [];
+    return this.getChunks(this.publicKeys);
   }
 
-  protected async fetchChunks(publicKeys: PublicKey[]): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
+  protected async getChunks(publicKeys: PublicKey[]): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
     const chunks = chunk(publicKeys, this.chunkSize);
-    const chunkPromises = chunks.map(chunk => this.fetchChunk(chunk));
+    const chunkPromises = chunks.map(chunk => this.getChunk(chunk));
     const resolvedChunks = await Promise.allSettled(chunkPromises);
 
     return resolvedChunks.flatMap((result) => (result.status === 'fulfilled' ? result.value : []));
   }
 
-  protected async fetchChunk(publicKeys: PublicKey[]): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
+  protected async getChunk(publicKeys: PublicKey[]): Promise<MaybeAccountInfoWithPublicKey<Buffer>[]> {
     try {
       // TODO: Use lower level RPC call to add dataSlice support.
       const accounts = (await this.connection.getMultipleAccountsInfo(publicKeys)) as (AccountInfo<Buffer> | null)[];
@@ -79,5 +87,16 @@ export class GmaBuilder {
       // TODO: Throw error instead?
       return publicKeys.map(publicKey => ({ pubkey: publicKey, exists: false }));
     }
+  }
+
+  protected boundNumber(n: number): number {
+    return this.boundIndex(n - 1) + 1;
+  }
+
+  protected boundIndex(index: number): number {
+    index = index < 0 ? 0 : index;
+    index = index >= this.publicKeys.length ? this.publicKeys.length - 1 : index;
+
+    return index;
   }
 }
