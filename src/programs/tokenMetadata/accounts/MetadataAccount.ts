58ce4ea626c6c3d2c67e3bbb16f49afc859cb4d7
Loris Leiva <loris.leiva@gmail.com>
2022-03-03 12:42:15 +0000

    wip

--- src/programs/tokenMetadata/accounts/MetadataAccount.ts
+++ src/programs/tokenMetadata/accounts/MetadataAccount.ts
@@ -1,14 +1,19 @@
 import { AccountInfo, PublicKey } from "@solana/web3.js";
-import { MetadataProgram } from "@metaplex-foundation/mpl-token-metadata";
-import { Account } from "@/programs/shared";
 import { Metadata } from "@/programs/tokenMetadata/generated";
+import { TokenMetadataProgram } from "@/programs/tokenMetadata";
+import { Account } from "@/programs/shared";
 import { JsonMetadata } from "@/modules/nfts";
 import { fetchJson, Pda } from "@/utils";
 
 export class MetadataAccount extends Account<Metadata> {
 
   static async pda(mint: PublicKey): Promise<Pda> {
-    return Pda.fromPromise(MetadataProgram.findMetadataAccount(mint));
+    // return Pda.fromPromise(MetadataProgram.findMetadataAccount(mint));
+    return Pda.find(TokenMetadataProgram.publicKey, [
+      Buffer.from('metadata', 'utf8'),
+      TokenMetadataProgram.publicKey.toBuffer(),
+      mint.toBuffer(),
+    ])
   }
 
   static fromAccountInfo(accountInfo: AccountInfo<Buffer>): MetadataAccount {
