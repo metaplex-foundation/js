c0f50d773f353731dfc2a6bddd6776b99f2102b1
Loris Leiva <loris.leiva@gmail.com>
2022-03-15 17:12:32 -0500

    wip

---
+++ .ammanrc.js
@@ -0,0 +1,32 @@
+const { LOCALHOST, tmpLedgerDir } = require('@metaplex-foundation/amman');
+const path = require('path');
+
+function localDeployPath(programName) {
+  return path.join(__dirname, '..', 'target', 'deploy', `${programName}.so`);
+}
+
+const programIds = {
+  metadata: 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+  vault: 'vau1zxA2LbssAUEF7Gpw91zMM1LvXrvpzJtmZ58rPsn',
+  auction: 'auctxRXPeJoc4817jDhf4HbjnhEcr1cCXenosMhK5R8',
+  metaplex: 'p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98',
+  fixedPriceSaleToken: 'SaLeTjyUa5wXHnGuewUSyJ5JWZaHwz3TxqUntCE9czo',
+  candyMachine: 'cndy3Z4yapfJBmL3ShUp5exZKqR3z33thTzeNMm2gRZ'
+};
+
+const programs = {
+  metadata: { programId: programIds.metadata, deployPath: localDeployPath('mpl_token_metadata') },
+};
+
+module.exports = {
+  validator: {
+    killRunningValidators: true,
+    programs,
+    jsonRpcUrl: LOCALHOST,
+    websocketUrl: '',
+    commitment: 'confirmed',
+    ledgerDir: tmpLedgerDir(),
+    resetLedger: true,
+    verifyFees: false,
+  }
+}
