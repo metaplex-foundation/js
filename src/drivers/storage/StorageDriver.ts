f7cbd31dfc35e7b99bbbb7dcf32dcdca4e28cbaa
Loris Leiva <loris.leiva@gmail.com>
2022-03-16 16:10:13 -0500

    wip

--- src/drivers/storage/StorageDriver.ts
+++ src/drivers/storage/StorageDriver.ts
@@ -5,4 +5,17 @@ import { MetaplexFile } from "../filesystem/MetaplexFile";
 export abstract class StorageDriver extends Driver {
   public abstract getPrice(file: MetaplexFile): Promise<BN>;
   public abstract upload(file: MetaplexFile): Promise<string>;
+
+  public async uploadJson<T extends object>(json: T): Promise<string> {
+    let jsonString;
+
+    try {
+      jsonString = JSON.stringify(json);
+    } catch (error) {
+      // TODO: Custom errors.
+      throw new Error('Invalid JSON');
+    }
+
+    return this.upload(new MetaplexFile(jsonString));
+  }
 }
--- test/modules/nfts/createNft.test.ts
+++ test/modules/nfts/createNft.test.ts
@@ -6,17 +6,22 @@ test('it can create an NFT with minimum configuration', async (t: Test) => {
 	// Given we have a Metaplex instance.
 	const mx = await metaplex();
 
+	// And we uploaded some metadata somewhere.
+	const uri = await mx.storage().uploadJson({
+		name: 'JSON NFT name'
+	})
+
 	// When we create a new NFT with minimum configuration.
 	const nft = await mx.nfts().createNft({
-		name: 'My NFT',
-		uri: 'https://example.org/123',
+		name: 'On-chain NFT name',
+		uri,
 	});
 
 	// Then we created and retrieved the new NFT and it has appropriate defaults.
 	spok(t, nft, {
 		$topic: 'Created NFT',
-		name: 'My NFT',
-		uri: 'https://example.org/123',
+		name: 'On-chain NFT name',
+		uri: uri,
 		json: null,
 		sellerFeeBasisPoints: 500,
 		primarySaleHappened: false,
