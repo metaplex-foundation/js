28f8de3e645060621537bac2c6ca0ce136735a9f
Loris Leiva <loris.leiva@gmail.com>
2022-03-16 16:37:20 -0500

    wip

--- src/drivers/storage/StorageDriver.ts
+++ src/drivers/storage/StorageDriver.ts
@@ -5,6 +5,8 @@ import { MetaplexFile } from "../filesystem/MetaplexFile";
 export abstract class StorageDriver extends Driver {
   public abstract getPrice(file: MetaplexFile): Promise<BN>;
   public abstract upload(file: MetaplexFile): Promise<string>;
+  public abstract download(uri: string): Promise<MetaplexFile>;
+  public abstract downloadJson<T extends object>(uri: string): Promise<T>;
 
   public async uploadJson<T extends object>(json: T): Promise<string> {
     let jsonString;
--- src/modules/nfts/actions/createNft.ts
+++ src/modules/nfts/actions/createNft.ts
@@ -3,11 +3,10 @@ import { getMinimumBalanceForRentExemptMint, getAssociatedTokenAddress } from "@
 import { bignum } from "@metaplex-foundation/beet";
 import { Metaplex } from "@/Metaplex";
 import { createNftBuilder } from "../transactionBuilders";
-import { MetadataAccount, MasterEditionAccount } from "@/programs/tokenMetadata";
+import { MetadataAccount, MasterEditionAccount, DataV2 } from "@/programs/tokenMetadata";
 import { Creator, Collection, Uses } from "@/programs/tokenMetadata/generated";
 import { Signer } from "@/utils";
 import { JsonMetadata } from "../models/JsonMetadata";
-import { MetaplexFile } from "@/drivers/Filesystem/MetaplexFile";
 
 export interface CreateNftParams {
   // Data.
@@ -48,12 +47,6 @@ export interface CreateNftResult {
 
 export const createNft = async (metaplex: Metaplex, params: CreateNftParams): Promise<CreateNftResult> => {
   const {
-    name,
-    symbol = '',
-    sellerFeeBasisPoints = 500,
-    creators = null,
-    collection = null,
-    uses = null,
     isMutable,
     maxSupply,
     allowHolderOffCurve = false,
@@ -67,17 +60,8 @@ export const createNft = async (metaplex: Metaplex, params: CreateNftParams): Pr
     associatedTokenProgram,
   } = params;
 
-  const resolvedUri = await getUri(metaplex, params);
-
-  const data = {
-    name,
-    symbol,
-    uri: resolvedUri,
-    sellerFeeBasisPoints,
-    creators,
-    collection,
-    uses,
-  }
+  const [uri, json] = await resolveUriAndJson(metaplex, params);
+  const data = resolveData(metaplex, params, uri, json);
 
   const metadata = await MetadataAccount.pda(mint.publicKey);
   const masterEdition = await MasterEditionAccount.pda(mint.publicKey);
@@ -117,20 +101,70 @@ export const createNft = async (metaplex: Metaplex, params: CreateNftParams): Pr
   };
 }
 
-export const getUri = async (metaplex: Metaplex, params: CreateNftParams): Promise<string> => {
+const resolveUriAndJson = async (metaplex: Metaplex, params: CreateNftParams): Promise<[string, JsonMetadata]> => {
   if (params.uri) {
-    return params.uri;
+    const json: JsonMetadata = await metaplex.storage().downloadJson(params.uri);
+
+    return [params.uri, json];
   }
 
   if (params.json) {
-    return metaplex.storage().upload(new MetaplexFile(JSON.stringify(params.json)));
+    const uri = await metaplex.storage().uploadJson(params.json);
+
+    return [uri, params.json];
   }
 
   const json: JsonMetadata = {
     name: params.name,
-    creators: params.creators,
-    //
+    symbol: params.symbol,
+    seller_fee_basis_points: params.sellerFeeBasisPoints,
+    properties: {
+      creators: params.creators?.map(creator => ({
+        address: creator.address.toBase58(),
+        share: creator.share,
+      })),
+    },
   };
 
-  return metaplex.storage().upload(new MetaplexFile(JSON.stringify(json)));
+  const uri = await metaplex.storage().uploadJson(json);
+
+  return [uri, json];
 };
+
+const resolveData = (metaplex: Metaplex, params: CreateNftParams, uri: string, json: JsonMetadata): DataV2 => {
+  const jsonCreators: Creator[] | undefined = json.properties?.creators
+    ?.filter(creator => creator.address)
+    .map(creator => ({
+      address: new PublicKey(creator.address as string),
+      share: creator.share ?? 0,
+      verified: false,
+    }));
+
+  let creators = params.creators ?? jsonCreators ?? null;
+
+  if (creators === null) {
+    creators = [{
+      address: metaplex.identity().publicKey,
+      share: 100,
+      verified: true,
+    }]
+  } else {
+    creators = creators.map(creator => {
+      if (creator.address.toBase58() === metaplex.identity().publicKey.toBase58()) {
+        return { ...creator, verified: true };
+      } else {
+        return creator;
+      }
+    });
+  }
+  
+  return {
+    name: params.name,
+    symbol: params.symbol ?? json.symbol ?? '',
+    uri,
+    sellerFeeBasisPoints: params.sellerFeeBasisPoints ?? json.seller_fee_basis_points ?? 500,
+    creators,
+    collection: params.collection ?? null,
+    uses: params.uses ?? null,
+  }
+}
