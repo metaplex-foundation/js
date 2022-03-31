2b5b07be1018188f4588fe94e1205e82228abcd8
Thorsten Lorenz <thlorenz@gmx.de>
2022-03-31 14:11:40 -0600

    chore: add metaplex plugin guard function

--- src/MetaplexPlugin.ts
+++ src/MetaplexPlugin.ts
@@ -1,5 +1,12 @@
+import { StorageDriver } from './drivers/storage';
 import { Metaplex } from './Metaplex';
 
 export type MetaplexPlugin = {
   install(metaplex: Metaplex): any;
 };
+
+export function isMetaplexPlugin(
+  pluginOrStorage: MetaplexPlugin | StorageDriver
+): pluginOrStorage is MetaplexPlugin {
+  return typeof (pluginOrStorage as MetaplexPlugin).install === 'function';
+}
