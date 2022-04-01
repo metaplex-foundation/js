4fccdf0548ed858bdfe0e263e8a90deaa262cddc
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-01 10:21:50 -0600

    chore: remove guard

--- src/MetaplexPlugin.ts
+++ src/MetaplexPlugin.ts
@@ -1,12 +1,5 @@
-import { StorageDriver } from './drivers/storage';
 import { Metaplex } from './Metaplex';
 
 export type MetaplexPlugin = {
   install(metaplex: Metaplex): any;
 };
-
-export function isMetaplexPlugin(
-  pluginOrStorage: MetaplexPlugin | StorageDriver
-): pluginOrStorage is MetaplexPlugin {
-  return typeof (pluginOrStorage as MetaplexPlugin).install === 'function';
-}
