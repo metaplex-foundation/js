663be156388400f28e8e081230597c6e35f6a16b
anoushk1234 <anoushka1828@gmail.com>
2022-04-06 22:01:29 +0530

    Added test for update nft

--- test/modules/nfts/updateNft.test.ts
+++ test/modules/nfts/updateNft.test.ts
@@ -1,24 +1,60 @@
 import test, { Test } from 'tape';
 import spok, { Specifications } from 'spok';
-import { PublicKey } from '@solana/web3.js';
-import { Nft } from '@/index';
+import { Nft, MetaplexFile, JsonMetadata } from '@/index';
 import { metaplex, spokSamePubkey } from '../../helpers';
 
-test('it can create an NFT with minimum configuration', async (t: Test) => {
+test('it can create and update the nft', async (t: Test) => {
   // Given we have a Metaplex instance.
-  const mx = await metaplex();
-  const nft_to_update = await mx.nfts().findNft({ mint: new PublicKey('some mint key') });
-  const new_name = 'some new name';
-  const new_uri = 'some new uri';
 
-  const { nft }: { nft: Nft } = await mx.nfts().updateNft(nft_to_update, {
-    name: new_name,
-    uri: new_uri,
+  const mx = await metaplex();
+  const imageFile = new MetaplexFile('some_image', 'some-image.jpg');
+  const imageUri = await mx.storage().upload(imageFile);
+
+  // And we uploaded some metadata containing this image.
+  const metadataUri = await mx.storage().uploadJson<JsonMetadata>({
+    name: 'JSON NFT name',
+    description: 'JSON NFT description',
+    image: imageUri,
   });
 
-  spok(t, nft, {
-    $topic: 'nft-update',
-    name: new_name, // Only added two update params but feel free to add more
-    uri: new_uri,
+  // When we create a new NFT with minimum configuration.
+  const { nft: createdNft } = await mx.nfts().createNft({
+    name: 'On-chain NFT name',
+    uri: metadataUri,
+  });
+
+  const updatedImageFile = new MetaplexFile('updated_image', 'updated-image.jpg');
+  const updatedImageUri: string = await mx.storage().upload(updatedImageFile);
+  const updatedMetadataUri: string = await mx.storage().uploadJson<JsonMetadata>({
+    name: 'Updated JSON NFT name',
+    description: 'Updated JSON NFT description',
+    image: updatedImageUri,
+  });
+  const { nft: updated_nft }: { nft: Nft } = await mx.nfts().updateNft(createdNft, {
+    name: 'Updated On-chain NFT name',
+    uri: updatedMetadataUri,
+  });
+
+  spok(t, updated_nft, {
+    $topic: 'update-nft',
+    name: 'Updated On-chain NFT name',
+    uri: updatedMetadataUri,
+    metadata: {
+      name: 'Updated JSON NFT name',
+      description: 'Updated JSON NFT description',
+      image: updatedImageUri,
+    },
+    sellerFeeBasisPoints: 500,
+    primarySaleHappened: false,
+    updateAuthority: spokSamePubkey(mx.identity().publicKey),
+    creators: [
+      {
+        address: spokSamePubkey(mx.identity().publicKey),
+        share: 100,
+        verified: true,
+      },
+    ],
+    collection: null,
+    uses: null,
   } as unknown as Specifications<Nft>);
 });
