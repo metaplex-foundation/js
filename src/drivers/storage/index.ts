8ec0559a99c4e019a0af0c018f82324bf339d757
Loris Leiva <loris.leiva@gmail.com>
2022-04-13 12:45:43 +0100

    Remove NftStorage provider
    It does not work with Webpack 4 and it wasn't fully implemented yet so let's remove it until we're ready for it.

--- src/drivers/storage/index.ts
+++ src/drivers/storage/index.ts
@@ -1,5 +1,4 @@
 export * from './AwsStorageDriver';
 export * from './BundlrStorageDriver';
 export * from './MockStorageDriver';
-export * from './NftStorageStorageDriver';
 export * from './StorageDriver';
