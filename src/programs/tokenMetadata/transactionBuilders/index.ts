b2488f36624179ed3eea79a543a6704a33e2e0ae
Loris Leiva <loris.leiva@gmail.com>
2022-04-21 11:43:04 +0100

    Create transaction builders

--- src/programs/tokenMetadata/transactionBuilders/index.ts
+++ src/programs/tokenMetadata/transactionBuilders/index.ts
@@ -1,3 +1,5 @@
 export * from './createMasterEditionV3Builder';
 export * from './createMetadataV2Builder';
 export * from './updateMetadataV2Builder';
+export * from './mintNewEditionFromMasterEditionViaTokenBuilder';
+export * from './mintNewEditionFromMasterEditionViaVaultProxyBuilder';
