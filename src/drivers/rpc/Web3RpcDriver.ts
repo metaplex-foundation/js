fe3047e5f1b41cb337e77576b3c873b8591fc7d8
Loris Leiva <loris.leiva@gmail.com>
2022-04-25 18:49:12 +0100

    Better fix

--- src/drivers/rpc/Web3RpcDriver.ts
+++ src/drivers/rpc/Web3RpcDriver.ts
@@ -36,6 +36,10 @@ export class Web3RpcDriver extends RpcDriver {
     transaction.feePayer ??= this.getDefaultFeePayer();
     transaction.recentBlockhash ??= await this.getLatestBlockhash();
 
+    if (transaction.feePayer && this.metaplex.identity().is(transaction.feePayer)) {
+      signers = [this.metaplex.identity(), ...signers];
+    }
+
     const { keypairs, identities } = getSignerHistogram(signers);
 
     if (keypairs.length > 0) {
