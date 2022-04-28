3733a6570c27280146f08c5e1e30506ab71dbe3f
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-28 12:24:53 -0500

    chore: more gate keeper docs

--- src/modules/candy-machine/models/config/Gatekeeper.ts
+++ src/modules/candy-machine/models/config/Gatekeeper.ts
@@ -4,6 +4,11 @@ import { PublicKeyString, tryConvertToPublickKey } from '../../../../shared';
 /**
  * Configures {@link CandyMachineConfig.gatekeeper} settings.
  *
+ * While the unpredictable mint index provides some protection against bots,
+ * they are still able to mint directly from the Candy Machine. If you want to
+ * make sure that only humans can mint from your project, gatekeeper settings
+ * can be enabled.
+ *
  * @property gatekeeperNetwork - Gateway provider address
  * @property expireOnUse - Requires a new gateway challenge after a use
  */
