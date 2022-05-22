2240512594584b703f80733dd46df9129bc260eb
Thorsten Lorenz <thlorenz@gmx.de>
2022-05-22 15:46:31 -0500

    chore: expose assets as base64 strings

--- test/plugins/candyMachineModule/helpers/assets.ts
+++ test/plugins/candyMachineModule/helpers/assets.ts
@@ -1,13 +1,11 @@
-import fs from 'fs';
-import path from 'path';
+import {
+  bench_80x80,
+  bridge_80x80,
+  rock_80x80,
+  walrus_80x80,
+} from '../../../fixtures/images';
 
-const fixtures = path.join(__dirname, '..', '..', 'fixtures');
-
-export const rockPng = fs.readFileSync(path.join(fixtures, 'rock_80x80.png'));
-export const bridgePng = fs.readFileSync(
-  path.join(fixtures, 'bridge_80x80.png')
-);
-export const walrusPng = fs.readFileSync(
-  path.join(fixtures, 'walrus_80x80.png')
-);
-export const benchPng = fs.readFileSync(path.join(fixtures, 'bench_80x80.png'));
+export const rockPng = Buffer.from(rock_80x80, 'base64');
+export const bridgePng = Buffer.from(bridge_80x80, 'base64');
+export const walrusPng = Buffer.from(walrus_80x80, 'base64');
+export const benchPng = Buffer.from(bench_80x80, 'base64');
