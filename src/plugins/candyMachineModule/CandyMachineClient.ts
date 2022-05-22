969807909da96aa445d5c5193919ee80e4184157
Thorsten Lorenz <thlorenz@gmx.de>
2022-05-22 10:07:01 -0500

    upload: multiple assets in sequence + paralle and optionally add to cm

--- src/plugins/candyMachineModule/CandyMachineClient.ts
+++ src/plugins/candyMachineModule/CandyMachineClient.ts
@@ -43,7 +43,11 @@ import {
   updateAuthorityOperation,
   UpdateAuthorityOutput,
 } from './updateAuthority';
-import { JsonMetadataFile, UploadMetadataInput } from '@/plugins/nftModule';
+import {
+  JsonMetadata,
+  JsonMetadataFile,
+  UploadMetadataInput,
+} from '@/plugins/nftModule';
 import { AddConfigLinesInput, addConfigLinesOperation } from './addConfigLines';
 
 export type CandyMachineInitFromConfigOpts = {
@@ -67,17 +71,47 @@ export type AddAssetsToCandyMachineParams = {
   // Transaction Options.
   confirmOptions?: ConfirmOptions;
 };
-export type UploadOneAssetToCandyMachineParams = UploadMetadataInput & {
+export type UploadAssetToCandyMachineParams = UploadMetadataInput & {
   // Accounts
   candyMachineAddress: PublicKey;
   authoritySigner: Signer;
 
-  // NFT
+  // Asset
   properties?: {
     creators?: Creator[];
     files?: JsonMetadataFile<MetaplexFile | string>[];
     [key: string]: unknown;
   };
+
+  // TODO(thlorenz):
+  // If `true` the successfully uploaded asset is added to the candy machine
+  addToCandyMachine?: boolean;
+
+  // Transaction Options.
+  confirmOptions?: ConfirmOptions;
+};
+
+// NOTE: the `[key: string]: unknown;` that is part of `UploadMetadataInput` breaks type safety once
+// we extend a derived type, thus we don't get intellisense for `CandyMachineAsset` either
+export type CandyMachineAsset = Omit<
+  UploadAssetToCandyMachineParams,
+  'candyMachineAddress' | 'authoritySigner'
+>;
+
+export type UploadAssetsToCandyMachineParams = {
+  // Accounts
+  candyMachineAddress: PublicKey;
+  authoritySigner: Signer;
+
+  // Assets
+  assets: CandyMachineAsset[];
+
+  // If `true` then the assets are uploaded in parallel, however note that this
+  // can result in some successfully uploading while others fail
+  parallel?: boolean;
+
+  // If `true` all successfully uploaded assets are added to the candy machine
+  addToCandyMachine?: boolean;
 
   // Transaction Options.
   confirmOptions?: ConfirmOptions;
@@ -279,9 +313,7 @@ export class CandyMachineClient extends ModuleClient {
     };
   }
 
-  async uploadOneAssetForCandyMachine(
-    params: UploadOneAssetToCandyMachineParams
-  ) {
+  async uploadAssetForCandyMachine(params: UploadAssetToCandyMachineParams) {
     const candyMachine = await this.findByAddress(params.candyMachineAddress);
     if (candyMachine == null) {
       throw new CandyMachineToUpdateNotFoundError(params.candyMachineAddress);
@@ -320,6 +352,101 @@ export class CandyMachineClient extends ModuleClient {
       candyMachine,
       metadata,
       uri,
+    };
+  }
+
+  async uploadAssetsForCandyMachine(params: UploadAssetsToCandyMachineParams) {
+    const candyMachine = await this.findByAddress(params.candyMachineAddress);
+    if (candyMachine == null) {
+      throw new CandyMachineToUpdateNotFoundError(params.candyMachineAddress);
+    }
+
+    assertNotFull(candyMachine, candyMachine.assetsCount);
+    assertCanAdd(candyMachine, candyMachine.assetsCount, params.assets.length);
+
+    const { parallel = false, addToCandyMachine = false } = params;
+
+    type UploadedAsset = {
+      uri: string;
+      metadata: JsonMetadata<string>;
+      name: string;
+    };
+    let uploadedAssets: UploadedAsset[] = [];
+    const errors = [];
+
+    if (parallel) {
+      // NOTE: we are uploading in parallel here but if only one upload was to fail
+      // all the other ones still happen as we cannot cancel promises
+      const promises = params.assets.map(async (asset) => {
+        let uploaded;
+        let err;
+        try {
+          uploaded = await this._uploadAssetAndSelectName(params, asset);
+        } catch (e) {
+          errors.push(e);
+        }
+
+        return { uploaded, err };
+      });
+
+      const results = await Promise.all(promises);
+      for (const { err, uploaded } of results) {
+        if (err) {
+          errors.push(err);
+        } else {
+          uploadedAssets.push(uploaded as UploadedAsset);
+        }
+      }
+    } else {
+      for (const asset of params.assets) {
+        try {
+          uploadedAssets.push(
+            await this._uploadAssetAndSelectName(params, asset)
+          );
+        } catch (err) {
+          errors.push(err);
+          continue;
+        }
+      }
+    }
+    const configLines: ConfigLine[] = uploadedAssets.map((x) => ({
+      uri: x.uri,
+      name: x.name,
+    }));
+
+    const {
+      transactionId,
+      candyMachine: updatedCandyMachine,
+      confirmResponse,
+    } = await this.addAssets({
+      ...params,
+      assets: configLines,
+    });
+
+    return {
+      addToCandyMachine,
+      transactionId,
+      candyMachine: updatedCandyMachine,
+      confirmResponse,
+    };
+  }
+
+  private async _uploadAssetAndSelectName(
+    params: UploadAssetsToCandyMachineParams,
+    asset: CandyMachineAsset
+  ) {
+    const { uri, metadata } = await this.uploadAssetForCandyMachine({
+      ...params,
+      ...asset,
+    });
+    return {
+      uri,
+      metadata,
+      name:
+        metadata.name ??
+        (typeof asset.name === 'string' ? asset.name : undefined) ??
+        metadata.description ??
+        uri,
     };
   }
 }
