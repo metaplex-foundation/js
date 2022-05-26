c4e3dc681f0de8b4f21c3bd598a723dcc501fbe8
Loris Leiva <loris.leiva@gmail.com>
2022-05-26 18:26:55 +0100

    chore: update aws storage driver

--- src/plugins/awsStorage/AwsStorageDriver.ts
+++ src/plugins/awsStorage/AwsStorageDriver.ts
@@ -1,43 +1,42 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Metaplex } from '@/Metaplex';
-import { StorageDriver, MetaplexFile } from '@/types';
-import { SolAmount } from '@/utils';
+import { StorageDriver, useLamports, Amount } from '@/types';
+import { MetaplexFile } from '../storageModule';
 
-export class AwsStorageDriver extends StorageDriver {
-  protected client: S3Client;
-  protected bucketName: string;
-
-  constructor(metaplex: Metaplex, client: S3Client, bucketName: string) {
-    super(metaplex);
-    this.client = client;
-    this.bucketName = bucketName;
-  }
-
-  public async getPrice(..._files: MetaplexFile[]): Promise<SolAmount> {
-    return SolAmount.zero();
-  }
-
-  public async upload(file: MetaplexFile): Promise<string> {
-    const command = new PutObjectCommand({
-      Bucket: this.bucketName,
-      Key: file.uniqueName,
-      Body: file.toBuffer(),
-    });
-
-    try {
-      await this.client.send(command);
-
-      return await this.getUrl(file.uniqueName);
-    } catch (err) {
-      // TODO: Custom errors.
-      throw err;
-    }
-  }
-
-  protected async getUrl(key: string) {
-    const region = await this.client.config.region();
+export const useAwsStorageDriver = (
+  metaplex: Metaplex,
+  client: S3Client,
+  bucketName: string
+): StorageDriver => {
+  const getUrl = async (key: string) => {
+    const region = await client.config.region();
     const encodedKey = encodeURIComponent(key);
 
-    return `https://s3.${region}.amazonaws.com/${this.bucketName}/${encodedKey}`;
-  }
-}
+    return `https://s3.${region}.amazonaws.com/${bucketName}/${encodedKey}`;
+  };
+
+  return {
+    metaplex,
+
+    getUploadPrice: async (_bytes: number): Promise<Amount> => {
+      return useLamports(0);
+    },
+
+    upload: async (file: MetaplexFile): Promise<string> => {
+      const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: file.uniqueName,
+        Body: file.toBuffer(),
+      });
+
+      try {
+        await client.send(command);
+
+        return await getUrl(file.uniqueName);
+      } catch (err) {
+        // TODO: Custom errors.
+        throw err;
+      }
+    },
+  };
+};
