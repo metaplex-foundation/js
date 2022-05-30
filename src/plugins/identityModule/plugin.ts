2f3cc5aa3fa663f74b15bf50427348b533744dec
Loris Leiva <loris.leiva@gmail.com>
2022-05-30 09:44:35 +0100

    chore: fix typo

--- src/plugins/identityModule/plugin.ts
+++ src/plugins/identityModule/plugin.ts
@@ -2,7 +2,7 @@ import type { Metaplex } from '@/Metaplex';
 import { MetaplexPlugin } from '@/types';
 import { IdentityClient } from './IdentityClient';
 
-export const storageModule = (): MetaplexPlugin => ({
+export const identityModule = (): MetaplexPlugin => ({
   install(metaplex: Metaplex) {
     const identityClient = new IdentityClient();
     metaplex.identity = () => identityClient;
