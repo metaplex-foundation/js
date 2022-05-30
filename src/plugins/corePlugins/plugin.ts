8dfc5688be2088f02cb116f6276f615aba6ac8ab
Loris Leiva <loris.leiva@gmail.com>
2022-05-30 14:07:49 +0100

    chore: register default drivers

--- src/plugins/corePlugins/plugin.ts
+++ src/plugins/corePlugins/plugin.ts
@@ -4,12 +4,23 @@ import { nftModule } from '../nftModule';
 import { candyMachineModule } from '../candyMachineModule';
 import { storageModule } from '../storageModule';
 import { identityModule } from '../identityModule';
+import { guestIdentity } from '../guestIdentity';
+import { bundlrStorage } from '../bundlrStorage';
 
 export const corePlugins = () => ({
   install(metaplex: Metaplex) {
+    // Low-level modules.
     metaplex.use(identityModule());
     metaplex.use(storageModule());
+
+    // Default drivers.
+    metaplex.use(guestIdentity());
+    metaplex.use(bundlrStorage());
+
+    // Register core programs.
     metaplex.use(corePrograms());
+
+    // Verticals.
     metaplex.use(nftModule());
     metaplex.use(candyMachineModule());
   },
