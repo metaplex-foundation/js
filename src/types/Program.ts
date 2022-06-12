33839d70de54efeda0873f921ef546fd1319293b
Loris Leiva <loris.leiva@gmail.com>
2022-06-12 17:22:29 +0100

    wip: playing with program types

--- src/types/Program.ts
+++ src/types/Program.ts
@@ -1,7 +1,13 @@
 import { PublicKey } from '@solana/web3.js';
 import { Metaplex } from '@/Metaplex';
 import { Cluster } from '@/types';
-import { GpaBuilder } from '@/utils';
+import { GpaBuilder, TransactionBuilder } from '@/utils';
+import {
+  Account,
+  MaybeAccount,
+  UnparsedAccount,
+  UnparsedMaybeAccount,
+} from './Account';
 
 export type ErrorWithLogs = Error & { logs: string[] };
 export type ErrorWithCode = Error & { code: number };
@@ -15,4 +21,24 @@ export type Program = {
   clusterFilter?: (cluster: Cluster) => boolean;
   errorResolver?: (error: ErrorWithLogs) => ErrorWithCode | null | undefined;
   gpaResolver?: (metaplex: Metaplex) => GpaBuilder;
+  accounts: <T extends ProgramAccounts = ProgramAccounts>() => T;
+  instructions: <T extends ProgramInstructions = ProgramInstructions>() => T;
+};
+
+export type ProgramAccounts = {
+  get: <T>(name: string) => ProgramAccountClient<T>;
+};
+
+export type ProgramAccountClient<T> = {
+  gpa: () => GpaBuilder;
+  find: (address: PublicKey) => Promise<MaybeAccount<T>>;
+  findAll: (addresses: PublicKey[]) => Promise<MaybeAccount<T>[]>;
+  parse: {
+    (unparsedAccount: UnparsedAccount): Account<T>;
+    (unparsedAccount: UnparsedMaybeAccount): MaybeAccount<T>;
+  };
+};
+
+export type ProgramInstructions = {
+  get: <T extends object>(name: string, args: T) => TransactionBuilder;
 };
