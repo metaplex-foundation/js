289c93f37417b551a421e2849a652a1fdc45cacc
Loris Leiva <loris.leiva@gmail.com>
2022-06-23 09:22:10 +0100

    chore: finish create AH with min config test

--- test/plugins/auctionHouseModule/createAuctionHouse.test.ts
+++ test/plugins/auctionHouseModule/createAuctionHouse.test.ts
@@ -1,5 +1,12 @@
 import test, { Test } from 'tape';
-import { metaplex, killStuckProcess } from '../../helpers';
+import spok from 'spok';
+import { metaplex, spokSamePubkey, killStuckProcess } from '../../helpers';
+import { WRAPPED_SOL_MINT } from '@/plugins/auctionHouseModule/constants';
+import {
+  findAuctionHouseFeePda,
+  findAuctionHousePda,
+  findAuctionHouseTreasuryPda,
+} from '@/programs';
 
 killStuckProcess();
 
@@ -7,12 +14,36 @@ test('[auctionHouseModule] create new Auction House with minimum configuration',
   // Given we have a Metaplex instance.
   const mx = await metaplex();
 
-  // When
-  const foo = await mx.auctions().createAuctionHouse({
+  // When we create a new Auction House with minimum configuration.
+  const auctionHouse = await mx.auctions().createAuctionHouse({
     sellerFeeBasisPoints: 200, // 2.00%
   });
 
-  // Then
-  console.log(foo);
-  console.log(foo.data.treasuryMint.toBase58());
+  // Then we created and returned the new Auction House and it has appropriate defaults.
+  const expectedCreator = mx.identity().publicKey;
+  const expectedMint = WRAPPED_SOL_MINT;
+  const expectedAddress = findAuctionHousePda(expectedCreator, expectedMint);
+  const expectedAuctionHouse = {
+    address: spokSamePubkey(expectedAddress),
+    creator: spokSamePubkey(expectedCreator),
+    treasuryMint: spokSamePubkey(expectedMint),
+    feeAccount: spokSamePubkey(findAuctionHouseFeePda(expectedAddress)),
+    treasuryAccount: spokSamePubkey(
+      findAuctionHouseTreasuryPda(expectedAddress)
+    ),
+    feeWithdrawalDestination: spokSamePubkey(expectedCreator),
+    treasuryWithdrawalDestination: spokSamePubkey(expectedCreator),
+  };
+
+  spok(t, auctionHouse, { $topic: 'AuctionHouse', ...expectedAuctionHouse });
+
+  // And we get the same result when we fetch the Auction House by address.
+  const retrievedAuctionHouse = await mx
+    .auctions()
+    .findAuctionHouseByAddress(auctionHouse.address);
+
+  spok(t, retrievedAuctionHouse, {
+    $topic: 'Retrieved AuctionHouse',
+    ...expectedAuctionHouse,
+  });
 });
