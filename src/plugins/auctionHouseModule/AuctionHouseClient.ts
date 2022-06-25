9fdcfdbf3229a61a764057865343d59b2297fd75
Loris Leiva <loris.leiva@gmail.com>
2022-06-25 17:08:10 +0100

    chore: add list method to AH client

--- src/plugins/auctionHouseModule/AuctionHouseClient.ts
+++ src/plugins/auctionHouseModule/AuctionHouseClient.ts
@@ -1,6 +1,14 @@
 import { Metaplex } from '@/Metaplex';
 import { Signer } from '@/types';
+import { Task } from '@/utils';
 import { AuctionHouse } from './AuctionHouse';
+import {
+  CreateListingInput,
+  createListingOperation,
+  CreateListingOutput,
+} from './createListing';
+
+type WithoutAH<T> = Omit<T, 'auctionHouse' | 'auctioneerAuthority'>;
 
 export class AuctionHouseClient {
   constructor(
@@ -9,5 +17,17 @@ export class AuctionHouseClient {
     protected readonly auctioneerAuthority?: Signer
   ) {}
 
-  //
+  list(input: WithoutAH<CreateListingInput>): Task<CreateListingOutput> {
+    const operation = createListingOperation(this.addAH(input));
+
+    return this.metaplex.operations().getTask(operation);
+  }
+
+  protected addAH<T>(input: WithoutAH<T>): T {
+    return {
+      auctionHouse: this.auctionHouse,
+      auctioneerAuthority: this.auctioneerAuthority,
+      ...input,
+    } as unknown as T;
+  }
 }
