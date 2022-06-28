7687f5c5cbf3dd8f7e976e73be0f289dacb5369b
Loris Leiva <loris.leiva@gmail.com>
2022-06-28 17:00:18 +0100

    wip

--- src/plugins/nftModule/plugin.ts
+++ src/plugins/nftModule/plugin.ts
@@ -31,11 +31,39 @@ import {
   uploadMetadataOperation,
   uploadMetadataOperationHandler,
 } from './uploadMetadata';
+import {
+  findMintWithMetadataByAddressOperation,
+  findMintWithMetadataByAddressOperationHandler,
+} from './findMintWithMetadataByAddress';
+import {
+  findMintWithMetadataByMetadataOperation,
+  findMintWithMetadataByMetadataOperationHandler,
+} from './findMintWithMetadataByMetadata';
+import {
+  findTokenWithMetadataByAddressOperation,
+  findTokenWithMetadataByAddressOperationHandler,
+} from './findTokenWithMetadataByAddress';
+import {
+  findTokenWithMetadataByMetadataOperation,
+  findTokenWithMetadataByMetadataOperationHandler,
+} from './findTokenWithMetadataByMetadata';
+import {
+  findTokenWithMetadataByMintOperation,
+  findTokenWithMetadataByMintOperationHandler,
+} from './findTokenWithMetadataByMint';
 
 export const nftModule = (): MetaplexPlugin => ({
   install(metaplex: Metaplex) {
     const op = metaplex.operations();
     op.register(createNftOperation, createNftOperationHandler);
+    op.register(
+      findMintWithMetadataByAddressOperation,
+      findMintWithMetadataByAddressOperationHandler
+    );
+    op.register(
+      findMintWithMetadataByMetadataOperation,
+      findMintWithMetadataByMetadataOperationHandler
+    );
     op.register(findNftByMintOperation, findNftByMintOnChainOperationHandler);
     op.register(
       findNftsByCandyMachineOperation,
@@ -52,6 +80,18 @@ export const nftModule = (): MetaplexPlugin => ({
     op.register(
       findNftsByOwnerOperation,
       findNftsByOwnerOnChainOperationHandler
+    );
+    op.register(
+      findTokenWithMetadataByAddressOperation,
+      findTokenWithMetadataByAddressOperationHandler
+    );
+    op.register(
+      findTokenWithMetadataByMetadataOperation,
+      findTokenWithMetadataByMetadataOperationHandler
+    );
+    op.register(
+      findTokenWithMetadataByMintOperation,
+      findTokenWithMetadataByMintOperationHandler
     );
     op.register(printNewEditionOperation, printNewEditionOperationHandler);
     op.register(updateNftOperation, updateNftOperationHandler);
--- src/plugins/tokenModule/TokenClient.ts
+++ src/plugins/tokenModule/TokenClient.ts
@@ -1,6 +1,9 @@
 import type { Metaplex } from '@/Metaplex';
 import type { Commitment, PublicKey } from '@solana/web3.js';
 import { findMintByAddressOperation } from './findMintByAddress';
+import { findTokenByAddressOperation } from './findTokenByAddress';
+import { findTokenWithMintByAddressOperation } from './findTokenWithMintByAddress';
+import { findTokenWithMintByMintOperation } from './findTokenWithMintByMint';
 import { TokenBuildersClient } from './TokenBuildersClient';
 
 export class TokenClient {
@@ -14,5 +17,31 @@ export class TokenClient {
     return this.metaplex
       .operations()
       .getTask(findMintByAddressOperation({ address, commitment }));
+  }
+
+  findTokenByAddress(address: PublicKey, commitment?: Commitment) {
+    return this.metaplex
+      .operations()
+      .getTask(findTokenByAddressOperation({ address, commitment }));
+  }
+
+  findTokenWithMintByAddress(address: PublicKey, commitment?: Commitment) {
+    return this.metaplex
+      .operations()
+      .getTask(findTokenWithMintByAddressOperation({ address, commitment }));
+  }
+
+  findTokenWithMintByMint(
+    mintAddress: PublicKey,
+    ownerAddress: PublicKey,
+    commitment?: Commitment
+  ) {
+    return this.metaplex.operations().getTask(
+      findTokenWithMintByMintOperation({
+        mintAddress,
+        ownerAddress,
+        commitment,
+      })
+    );
   }
 }
