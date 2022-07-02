4bb93fe6f424bc4830fd2d498b54462dca3d39cc
Loris Leiva <loris.leiva@gmail.com>
2022-07-02 16:29:40 +0100

    wip

--- test/plugins/candyMachineModule/updateCandyMachine.test.ts
+++ test/plugins/candyMachineModule/updateCandyMachine.test.ts
@@ -1,5 +1,10 @@
 import test from 'tape';
-import spok from 'spok';
+import spok, { Specifications } from 'spok';
+import {
+  CandyMachineData,
+  EndSettingType,
+  WhitelistMintMode,
+} from '@metaplex-foundation/mpl-candy-machine';
 import {
   amman,
   killStuckProcess,
@@ -7,40 +12,80 @@ import {
   spokSameBignum,
   spokSamePubkey,
 } from '../../helpers';
-import {
-  CandyMachineData,
-  EndSettingType,
-  WhitelistMintMode,
-} from '@metaplex-foundation/mpl-candy-machine';
-import { CandyMachine } from '@/plugins';
+import { CandyMachine, sol } from '@/index';
+import { createCandyMachine } from './helpers';
+import { Keypair } from '@solana/web3.js';
 
 killStuckProcess();
 
-function assertProperlyUpdatedScalars(
-  t: test.Test,
-  createdCandyMachine: CandyMachine,
-  updatedCandyMachine: CandyMachine,
-  expectedChanges: Partial<Record<keyof CandyMachine, any>>
-) {
-  const expected: Partial<Record<keyof CandyMachine, any>> = {
-    ...createdCandyMachine,
-    ...expectedChanges,
-  };
-
-  spok(t, updatedCandyMachine, {
-    uuid: expected.uuid,
-
-    price: spokSameBignum(expected.price),
-    symbol: expected.symbol,
-    sellerFeeBasisPoints: expected.sellerFeeBasisPoints,
-
-    maxSupply: spokSameBignum(expected.maxSupply),
-    isMutable: expected.isMutable,
-    retainAuthority: expected.retainAuthority,
-    goLiveDate: spokSameBignum(expected.goLiveDate),
-    itemsAvailable: expected.itemsAvailable,
+test.only('[candyMachineModule] update candy machine data', async (t) => {
+  // Given an existing Candy Machine.
+  const mx = await metaplex();
+  const { candyMachine } = await createCandyMachine(mx, {
+    price: sol(1),
+    sellerFeeBasisPoints: 100,
+    itemsAvailable: 100,
+    symbol: 'OLD',
+    maxEditionSupply: 0,
+    isMutable: true,
+    retainAuthority: true,
+    goLiveDate: 1000000000,
+    creators: mx.identity().publicKey,
+    // TODO: below
+    // endSettings?: Option<EndSettings>; // Defaults to null.
+    // hiddenSettings?: Option<HiddenSettings>; // Defaults to null.
+    // whitelistMintSettings?: Option<WhitelistMintSettings>; // Defaults to null.
+    // gatekeeper?: Option<GatekeeperConfig>; // Defaults to null.
   });
-}
+
+  // When we update the Candy Machine with the following data.
+  const creatorA = Keypair.generate();
+  const creatorB = Keypair.generate();
+  const { candyMachine: updatedCandyMachine } = await mx
+    .candyMachines()
+    .update(candyMachine, {
+      authority: mx.identity(),
+      price: sol(2),
+      sellerFeeBasisPoints: 200,
+      itemsAvailable: 100, // <- Can only be updated with hidden settings.
+      symbol: 'NEW',
+      maxEditionSupply: 1,
+      isMutable: false,
+      retainAuthority: false,
+      goLiveDate: 2000000000,
+      creators: [
+        { address: creatorA.publicKey, verified: false, share: 50 },
+        { address: creatorB.publicKey, verified: false, share: 50 },
+      ],
+    })
+    .run();
+
+  // Then the Candy Machine has been updated properly.
+  spok(t, updatedCandyMachine, {
+    $topic: 'Updated Candy Machine',
+    authorityAddress: spokSamePubkey(mx.identity().publicKey),
+    price: sol(2),
+    sellerFeeBasisPoints: 200,
+    itemsAvailable: 100,
+    symbol: 'NEW',
+    maxEditionSupply: spokSameBignum(1),
+    isMutable: false,
+    retainAuthority: false,
+    goLiveDate: spokSameBignum(2000000000),
+    creators: [
+      {
+        address: spokSamePubkey(creatorA.publicKey),
+        verified: false,
+        share: 50,
+      },
+      {
+        address: spokSamePubkey(creatorB.publicKey),
+        verified: false,
+        share: 50,
+      },
+    ],
+  } as unknown as Specifications<CandyMachine>);
+});
 
 // -----------------
 // Scalar Properties
