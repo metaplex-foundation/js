413ff95255de0e72c248f6d9af6355af7cf903b3
Loris Leiva <loris.leiva@gmail.com>
2022-07-07 00:07:14 +0100

    wip

--- test/plugins/nftModule/findNftByMint.test.ts
+++ test/plugins/nftModule/findNftByMint.test.ts
@@ -11,26 +11,30 @@ test('[nftModule] it can fetch an NFT by its mint address', async (t: Test) => {
   const nft = await createNft(mx, { name: 'Some NFT' }, { mint });
 
   // When we fetch that NFT using its mint address.
-  const fetchedNft = await mx.nfts().findByMint(mint.publicKey);
+  const fetchedNft = await mx.nfts().findByMint(mint.publicKey).run();
 
   // Then we get the right NFT.
-  t.true(fetchedNft.equals(nft));
+  t.same(fetchedNft, nft);
 });
 
 test('[nftModule] it can fetch an NFT with an invalid URI', async (t: Test) => {
   // Given an existing NFT with an invalid URI.
   const mx = await metaplex();
-  const { nft } = await mx.nfts().create({
-    uri: 'https://example.com/some/invalid/uri',
-  });
+  const { nft } = await mx
+    .nfts()
+    .create({
+      name: 'Some NFT',
+      sellerFeeBasisPoints: 200,
+      uri: 'https://example.com/some/invalid/uri',
+    })
+    .run();
 
   // When we fetch that NFT using its mint address.
-  const fetchedNft = await mx.nfts().findByMint(nft.mint);
+  const fetchedNft = await mx.nfts().findByMint(nft.mintAddress).run();
 
   // Then we get the right NFT.
-  t.true(fetchedNft.equals(nft));
+  t.same(fetchedNft.mintAddress, nft.mintAddress);
 
   // And its metadata is empty.
-  t.same(fetchedNft.metadata, {});
-  t.equals(fetchedNft.metadataTask.getStatus(), 'failed');
+  t.same(fetchedNft.json, null);
 });
--- test/plugins/nftModule/findNftsByCreator.test.ts
+++ test/plugins/nftModule/findNftsByCreator.test.ts
@@ -14,22 +14,22 @@ test('[nftModule] it can fetch all NFTs from the first creator', async (t: Test)
   const nftB = await createNftWithFirstCreator(mx, 'NFT B', creatorB);
 
   // When we fetch the NFTs by creator A.
-  const nftsA = await mx.nfts().findAllByCreator(creatorA);
+  const nftsA = await mx.nfts().findAllByCreator(creatorA).run();
 
   // Then we don't get the NFTs from creator B.
   t.same(
     nftsA.map((nft) => nft.name),
     ['NFT A']
   );
-  t.true(nftsA[0].equals(nftA));
+  t.same(nftsA[0].mintAddress, nftA.mintAddress);
 
   // And vice versa.
-  const nftsB = await mx.nfts().findAllByCreator(creatorB);
+  const nftsB = await mx.nfts().findAllByCreator(creatorB).run();
   t.same(
     nftsB.map((nft) => nft.name),
     ['NFT B']
   );
-  t.true(nftsB[0].equals(nftB));
+  t.same(nftsB[0].mintAddress, nftB.mintAddress);
 });
 
 test('[nftModule] it can fetch all NFTs from other creator positions', async (t: Test) => {
@@ -41,22 +41,28 @@ test('[nftModule] it can fetch all NFTs from other creator positions', async (t:
   const nftB = await createNftWithSecondCreator(mx, 'NFT B', creatorB);
 
   // When we fetch the NFTs by second creator A.
-  const nftsA = await mx.nfts().findAllByCreator(creatorA, 2);
+  const nftsA = await mx
+    .nfts()
+    .findAllByCreator(creatorA, { position: 2 })
+    .run();
 
   // Then we don't get the NFTs from second creator B.
   t.same(
     nftsA.map((nft) => nft.name),
     ['NFT A']
   );
-  t.true(nftsA[0].equals(nftA));
+  t.same(nftsA[0].mintAddress, nftA.mintAddress);
 
   // And vice versa.
-  const nftsB = await mx.nfts().findAllByCreator(creatorB, 2);
+  const nftsB = await mx
+    .nfts()
+    .findAllByCreator(creatorB, { position: 2 })
+    .run();
   t.same(
     nftsB.map((nft) => nft.name),
     ['NFT B']
   );
-  t.true(nftsB[0].equals(nftB));
+  t.same(nftsB[0].mintAddress, nftB.mintAddress);
 });
 
 const createNftWithFirstCreator = (
@@ -66,8 +72,9 @@ const createNftWithFirstCreator = (
 ) => {
   return createNft(
     mx,
-    { name },
+    {},
     {
+      name,
       creators: [
         { address: creator, share: 50, verified: false },
         { address: mx.identity().publicKey, share: 50, verified: true },
@@ -83,8 +90,9 @@ const createNftWithSecondCreator = (
 ) => {
   return createNft(
     mx,
-    { name },
+    {},
     {
+      name,
       creators: [
         { address: mx.identity().publicKey, share: 50, verified: true },
         { address: creator, share: 50, verified: false },
