21d883bf84c11b1fdc873e9d033a7aa33e8eb933
Kyrell Dixon <kyrell.brown@gmail.com>
2022-07-07 15:55:35 -0500

    test(use-nft): add more tests to use nft

--- test/plugins/nftModule/useNft.test.ts
+++ test/plugins/nftModule/useNft.test.ts
@@ -1,7 +1,8 @@
 import test, { Test } from 'tape';
 import spok, { Specifications } from 'spok';
-import { Nft, useMetaplexFile } from '@/index';
+import { MetaplexError, Nft, useMetaplexFile } from '@/index';
 import { UseMethod } from '@metaplex-foundation/mpl-token-metadata';
+import { Keypair } from '@solana/web3.js';
 import {
   metaplex,
   createNft,
@@ -110,3 +111,71 @@ test('it can use an nft multiple times', async (t: Test) => {
     },
   } as unknown as Specifications<Nft>);
 });
+
+test('it only allows the owner to update the uses', async (t: Test) => {
+  // Given we have a Metaplex instance.
+  const mx = await metaplex();
+
+  // And an existing SFT owned by owner.
+  const owner = Keypair.generate().publicKey;
+
+  const nft = await createNft(
+    mx,
+    {
+      name: 'JSON SFT name',
+      description: 'JSON SFT description',
+      image: useMetaplexFile('some image', 'some-image.jpg'),
+    },
+    {
+      name: 'On-chain SFT name',
+      owner,
+      isMutable: true,
+      uses: {
+        useMethod: UseMethod.Multiple,
+        remaining: 10,
+        total: 10,
+      },
+    }
+  );
+
+  // When the wrong owner uses the NFT
+  try {
+    await mx.nfts().use(nft);
+  } catch (error) {
+    // Then we should get a MetaplexError.
+    t.equal(error instanceof MetaplexError, true);
+  }
+});
+
+test('it can only be used up to the total uses', async (t: Test) => {
+  // Given we have a Metaplex instance.
+  const mx = await metaplex();
+
+  // And an existing SFT with no remaining uses.
+  const nft = await createNft(
+    mx,
+    {
+      name: 'JSON SFT name',
+      description: 'JSON SFT description',
+      image: useMetaplexFile('some image', 'some-image.jpg'),
+    },
+    {
+      name: 'On-chain SFT name',
+      isMutable: true,
+      uses: {
+        useMethod: UseMethod.Multiple,
+        remaining: 1,
+        total: 10,
+      },
+    }
+  );
+
+  // It throws an error when there are no remaining uses.
+  try {
+    await mx.nfts().use(nft, { numberOfUses: 2 });
+  } catch (error) {
+    t.match((error as MetaplexError).problem, /not enough uses/i);
+  }
+});
+
+test;
