4390ae6c0b7d2179c10e9dc4d37cf48ccc6c3a8c
Loris Leiva <loris.leiva@gmail.com>
2022-07-25 14:51:35 +0100

    chore: add test for upload progress

--- packages/js-plugin-nft-storage/test/NftStorageDriver.test.ts
+++ packages/js-plugin-nft-storage/test/NftStorageDriver.test.ts
@@ -1,11 +1,11 @@
 import { toMetaplexFile } from '@metaplex-foundation/js';
 import test, { Test } from 'tape';
 import { killStuckProcess, metaplex } from './helpers';
-import { nftStorage } from '../src';
+import { nftStorage, NftStorageDriver } from '../src';
 
 killStuckProcess();
 
-test('[nftStorage] it can upload one file to NFT Storage', async (t: Test) => {
+test('[nftStorage] it can upload one file', async (t: Test) => {
   // Given a Metaplex instance using NFT.Storage.
   const mx = await metaplex();
   mx.use(nftStorage());
@@ -31,7 +31,7 @@ test('[nftStorage] it can upload one file to NFT Storage', async (t: Test) => {
   );
 });
 
-test('[nftStorage] it can upload one file to NFT Storage without a Gateway URL', async (t: Test) => {
+test('[nftStorage] it can upload one file without a Gateway URL', async (t: Test) => {
   // Given a Metaplex instance using NFT.Storage without Gateway URLs.
   const mx = await metaplex();
   mx.use(nftStorage({ useGatewayUrls: false }));
@@ -47,7 +47,7 @@ test('[nftStorage] it can upload one file to NFT Storage without a Gateway URL',
   console.log(uri);
 });
 
-test('[nftStorage] it can upload multiple files in batch to NFT Storage', async (t: Test) => {
+test('[nftStorage] it can upload multiple files in batch', async (t: Test) => {
   // Given a Metaplex instance using NFT.Storage with a batch size of 1.
   const mx = await metaplex();
   mx.use(nftStorage({ batchSize: 1 }));
@@ -74,4 +74,24 @@ test('[nftStorage] it can upload multiple files in batch to NFT Storage', async 
     'some-image-B',
     'should return the second asset'
   );
+});
+
+test('[nftStorage] it can keep track of upload progress', async (t: Test) => {
+  // Given a Metaplex instance using NFT.Storage.
+  const mx = await metaplex();
+  mx.use(nftStorage());
+
+  // And a progress callback that counts the stored chunks.
+  let chunkCounter = 0;
+  const driver = mx.storage().driver() as NftStorageDriver;
+  driver.onProgress((_size: number) => chunkCounter++);
+
+  // When we upload some asset with a size of 3 chunks.
+  const MAX_CHUNK_SIZE = 10_000_000;
+  await mx
+    .storage()
+    .upload(toMetaplexFile('x'.repeat(MAX_CHUNK_SIZE * 3), 'some-image.jpg'));
+
+  // Then the progress callback should be called 3 times.
+  t.equals(chunkCounter, 3, 'should emit 3 progress events');
 });
