b0ccd91fa102dc0ba4823aefb7f42458a0bba51b
Loris Leiva <loris.leiva@gmail.com>
2022-07-30 09:55:45 +0100

    wip

--- packages/js/test/plugins/nftModule/findNftByMint.test.ts
+++ packages/js/test/plugins/nftModule/findNftByMint.test.ts
@@ -1,6 +1,11 @@
 import { Keypair } from '@solana/web3.js';
 import test, { Test } from 'tape';
-import { metaplex, createNft, killStuckProcess } from '../../helpers';
+import {
+  metaplex,
+  createNft,
+  killStuckProcess,
+  createSft,
+} from '../../helpers';
 
 killStuckProcess();
 
@@ -13,11 +18,32 @@ test('[nftModule] it can fetch an NFT by its mint address', async (t: Test) => {
     json: { name: 'Some NFT' },
   });
 
-  // When we fetch that NFT using its mint address.
-  const fetchedNft = await mx.nfts().findByMint(mint.publicKey).run();
+  // When we fetch that NFT using its mint address and its token address.
+  const fetchedNft = await mx
+    .nfts()
+    .findByMint(mint.publicKey, {
+      tokenAddress: nft.token.address,
+    })
+    .run();
 
   // Then we get the right NFT.
   t.same(fetchedNft, nft);
+});
+
+test('[nftModule] it can fetch an SFT by its mint address', async (t: Test) => {
+  // Given a metaplex instance and an existing SFT.
+  const mx = await metaplex();
+  const mint = Keypair.generate();
+  const sft = await createSft(mx, {
+    useNewMint: mint,
+    json: { name: 'Some SFT' },
+  });
+
+  // When we fetch that SFT using its mint address.
+  const fetchedSft = await mx.nfts().findByMint(mint.publicKey).run();
+
+  // Then we get the right SFT.
+  t.same(fetchedSft, sft);
 });
 
 test('[nftModule] it can fetch an NFT with an invalid URI', async (t: Test) => {
