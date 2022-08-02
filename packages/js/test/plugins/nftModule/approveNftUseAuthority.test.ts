82997a27b451a990936608cc9f56c4741825cbb5
Loris Leiva <loris.leiva@gmail.com>
2022-08-02 17:59:48 +0100

    wip

--- packages/js/test/plugins/nftModule/approveNftUseAuthority.test.ts
+++ packages/js/test/plugins/nftModule/approveNftUseAuthority.test.ts
@@ -4,6 +4,7 @@ import { Keypair } from '@solana/web3.js';
 import spok, { Specifications } from 'spok';
 import test, { Test } from 'tape';
 import {
+  assertThrows,
   createNft,
   killStuckProcess,
   metaplex,
@@ -12,7 +13,7 @@ import {
 
 killStuckProcess();
 
-test('[nftModule] it can approve a Use authority for a given Nft', async (t: Test) => {
+test('[nftModule] it can approve a use authority for a given Nft', async (t: Test) => {
   // Given we have a Metaplex instance and a usable NFT.
   const mx = await metaplex();
   const nft = await createNft(mx, {
@@ -52,4 +53,41 @@ test('[nftModule] it can approve a Use authority for a given Nft', async (t: Tes
       total: spokSameBignum(10),
     },
   } as unknown as Specifications<Nft>);
+});
+
+test('[nftModule] approve use authorities cannot use more than the agreed amount', async (t: Test) => {
+  // Given we have a Metaplex instance and a usable NFT with 10 remaining uses.
+  const mx = await metaplex();
+  const nft = await createNft(mx, {
+    uses: {
+      useMethod: UseMethod.Multiple,
+      remaining: 10,
+      total: 10,
+    },
+  });
+
+  // And a use authority has been approved for 5 uses only.
+  const currentUser = Keypair.generate();
+  await mx
+    .nfts()
+    .approveUseAuthority(nft, currentUser.publicKey, {
+      numberOfUses: 5,
+    })
+    .run();
+
+  // When we try to use that authority for 6 uses.
+  const promise = mx
+    .nfts()
+    .use(nft, {
+      useAuthority: currentUser,
+      numberOfUses: 6,
+    })
+    .run();
+
+  // Then we should get an error.
+  await assertThrows(
+    t,
+    promise,
+    /There are not enough Uses left on this token/
+  );
 });
