e855a138372da9b6b47a3c9d8322266e744640c8
Loris Leiva <loris.leiva@gmail.com>
2022-08-18 15:52:48 +0100

    chore: use recommended setup for monorepos

--- packages/js-plugin-nft-storage/src/index.ts
+++ packages/js-plugin-nft-storage/src/index.ts
@@ -1,9 +1,3 @@
-/**
- * This JS SDK plugin allows you to use NFT Storage as a Storage Driver.
- *
- * @module js-plugin-nft-storage
- */
-
 export * from './NftStorageDriver';
 export * from './plugin';
 export * from './utils';
--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,9 +1,3 @@
-/**
- * This is the main package for the JS SDK.
- *
- * @module js
- */
-
 export * from './errors';
 export * from './plugins';
 export * from './types';
