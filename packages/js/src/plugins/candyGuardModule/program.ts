0a46c0981d5366cce67b2f8dabe118ced274d710
Loris Leiva <loris.leiva@gmail.com>
2022-09-05 18:37:54 +0100

    chore: add error resolver for candy guard

--- packages/js/src/plugins/candyGuardModule/program.ts
+++ packages/js/src/plugins/candyGuardModule/program.ts
@@ -1,5 +1,9 @@
-import { Program } from '@/types';
-import { PROGRAM_ID } from '@metaplex-foundation/mpl-candy-guard';
+import { ErrorWithLogs, Program } from '@/types';
+import {
+  PROGRAM_ID,
+  errorFromCode,
+} from '@metaplex-foundation/mpl-candy-guard';
+import { initCusper } from '@metaplex-foundation/cusper';
 
 export type CandyGuardProgram = Program & { availableGuards: string[] };
 
@@ -7,6 +11,8 @@ export type CandyGuardProgram = Program & { availableGuards: string[] };
 export const DefaultCandyGuardProgram: CandyGuardProgram = {
   name: 'CandyGuardProgram',
   address: PROGRAM_ID,
+  errorResolver: (error: ErrorWithLogs) =>
+    initCusper(errorFromCode).errorFromProgramLogs(error.logs, false),
   availableGuards: [
     'bot_tax',
     'live_date',
