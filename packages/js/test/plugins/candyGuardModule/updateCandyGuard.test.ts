bf71fa2021427a5b3bdf0b5b88f5a106d200efd6
Loris Leiva <loris.leiva@gmail.com>
2022-09-06 10:38:54 +0100

    wip

--- packages/js/test/plugins/candyGuardModule/updateCandyGuard.test.ts
+++ packages/js/test/plugins/candyGuardModule/updateCandyGuard.test.ts
@@ -3,14 +3,10 @@ import {
   DefaultCandyGuardSettings,
   emptyDefaultCandyGuardSettings,
   sol,
-  toBigNumber,
   toDateTime,
   token,
 } from '@/index';
-import {
-  EndSettingType,
-  WhitelistTokenMode,
-} from '@metaplex-foundation/mpl-candy-guard';
+import { EndSettingType } from '@metaplex-foundation/mpl-candy-guard';
 import { Keypair } from '@solana/web3.js';
 import spok, { Specifications } from 'spok';
 import test from 'tape';
@@ -151,21 +147,32 @@ test('[candyGuardModule] it can update groups', async (t) => {
   } as unknown as Specifications<CandyGuard<DefaultCandyGuardSettings>>);
 });
 
-test.skip('[candyGuardModule] it overrides all previous groups', async (t) => {
+test('[candyGuardModule] it overrides all previous groups', async (t) => {
   // Given a Metaplex instance.
   const mx = await metaplex();
 
-  // And an existing Candy Guard with one group.
+  // And an existing Candy Guard with three groups.
+  const splTokenA = Keypair.generate().publicKey;
   const candyGuard = await createCandyGuard(mx, {
     guards: {},
     groups: [
       {
-        liveDate: { date: toDateTime('2022-09-06T10:00:00.000Z') },
+        liveDate: { date: null },
+        lamports: { amount: sol(1) },
+      },
+      {
+        liveDate: { date: toDateTime('2022-09-06T12:00:00.000Z') },
+        splToken: { tokenMint: splTokenA, amount: token(375) },
+      },
+      {
+        liveDate: { date: toDateTime('2022-09-06T13:00:00.000Z') },
+        lamports: { amount: sol(2) },
       },
     ],
   });
 
-  // When we update the guards of the only Candy Guard's group.
+  // When we update the candy guard's groups.
+  const splTokenB = Keypair.generate().publicKey;
   await mx
     .candyGuards()
     .update({
@@ -173,13 +180,18 @@ test.skip('[candyGuardModule] it overrides all previous groups', async (t) => {
       guards: {},
       groups: [
         {
+          liveDate: { date: null },
+          lamports: { amount: sol(2) },
+        },
+        {
           liveDate: { date: toDateTime('2022-09-06T12:00:00.000Z') },
+          splToken: { tokenMint: splTokenB, amount: token(42) },
         },
       ],
     })
     .run();
 
-  // Then the updated Candy Guard has the expected data.
+  // Then all previous groups were overridden.
   const updatedCandyGuard = await mx.candyGuards().refresh(candyGuard).run();
   spok(t, updatedCandyGuard, {
     $topic: 'Updated Candy Guard',
@@ -189,14 +201,70 @@ test.skip('[candyGuardModule] it overrides all previous groups', async (t) => {
     groups: [
       {
         ...emptyDefaultCandyGuardSettings,
+        liveDate: { date: null },
+        lamports: { amount: spokSameAmount(sol(2)) },
+      },
+      {
+        ...emptyDefaultCandyGuardSettings,
         liveDate: {
           date: spokSameBignum(toDateTime('2022-09-06T12:00:00.000Z')),
+        },
+        splToken: {
+          tokenMint: spokSamePubkey(splTokenB),
+          amount: spokSameAmount(token(42)),
         },
       },
     ],
   } as unknown as Specifications<CandyGuard<DefaultCandyGuardSettings>>);
 });
 
-test.skip('[candyGuardModule] it can remove all guards and groups', async (t) => {
-  // ...
+test('[candyGuardModule] it can remove all guards and groups', async (t) => {
+  // Given a Metaplex instance.
+  const mx = await metaplex();
+
+  // And an existing Candy Guard with three groups and some default guards.
+  const splTokenA = Keypair.generate().publicKey;
+  const candyGuard = await createCandyGuard(mx, {
+    guards: {
+      botTax: { lamports: sol(0.01), lastInstruction: true },
+      endSettings: {
+        endSettingType: EndSettingType.Date,
+        date: toDateTime('2022-09-06T10:00:00.000Z'),
+      },
+    },
+    groups: [
+      {
+        liveDate: { date: null },
+        lamports: { amount: sol(1) },
+      },
+      {
+        liveDate: { date: toDateTime('2022-09-06T12:00:00.000Z') },
+        splToken: { tokenMint: splTokenA, amount: token(375) },
+      },
+      {
+        liveDate: { date: toDateTime('2022-09-06T13:00:00.000Z') },
+        lamports: { amount: sol(2) },
+      },
+    ],
+  });
+
+  // When we update the candy guard with no guards and no groups.
+  await mx
+    .candyGuards()
+    .update({
+      candyGuard: candyGuard.address,
+      guards: {},
+      groups: [],
+    })
+    .run();
+
+  // Then all groups and default guards were removed.
+  const updatedCandyGuard = await mx.candyGuards().refresh(candyGuard).run();
+  spok(t, updatedCandyGuard, {
+    $topic: 'Updated Candy Guard',
+    model: 'candyGuard',
+    address: spokSamePubkey(candyGuard.address),
+    guards: emptyDefaultCandyGuardSettings,
+    groups: [],
+  } as unknown as Specifications<CandyGuard<DefaultCandyGuardSettings>>);
 });
