1c6363f6606c2e82a60b0d2fc7015f49f3b44c1f
Loris Leiva <loris.leiva@gmail.com>
2022-09-13 15:34:32 +0100

    wip

--- packages/js/test/plugins/candyMachineModule/insertCandyMachineItems.test.ts
+++ packages/js/test/plugins/candyMachineModule/insertCandyMachineItems.test.ts
@@ -6,11 +6,11 @@ import {
 import { Keypair } from '@solana/web3.js';
 import test from 'tape';
 import { assertThrows, killStuckProcess, metaplex } from '../../helpers';
-import { createCandyMachine } from './helpers';
+import { create32BitsHash, createCandyMachine } from './helpers';
 
 killStuckProcess();
 
-test.only('[candyMachineModule] it can add items to a candy machine', async (t) => {
+test('[candyMachineModule] it can add items to a candy machine', async (t) => {
   // Given an existing Candy Machine with a capacity of 100 items.
   const mx = await metaplex();
   const authority = Keypair.generate();
@@ -56,7 +56,84 @@ test.only('[candyMachineModule] it can add items to a candy machine', async (t) 
   ]);
 });
 
-test('[candyMachineModule] it cannot add items that would make the candy machine exceed the maximum capacity', async (t) => {
+test('[candyMachineModule] it uses the names and URIs as suffixes when adding items to a candy machine', async (t) => {
+  // Given an existing Candy Machine with prefixes for the names and URIs.
+  const mx = await metaplex();
+  const candyMachine = await createCandyMachine(mx, {
+    itemsAvailable: toBigNumber(9), // Numbers go from 1 to 9.
+    itemSettings: {
+      type: 'configLines',
+      prefixName: 'Degen #',
+      nameLength: 1, // E.g. "1".
+      prefixUri: 'https://example.com/degen/',
+      uriLength: 6, // E.g. "1.json".
+      isSequential: false,
+    },
+  });
+
+  // When we add two items to the Candy Machine by providing only the suffixes.
+  await mx
+    .candyMachines()
+    .insertItems({
+      candyMachine,
+      items: [
+        { name: '1', uri: '1.json' },
+        { name: '2', uri: '2.json' },
+      ],
+    })
+    .run();
+
+  // Then the update Candy Machine returns the full item names and URIs.
+  const updatedCandyMachine = await mx
+    .candyMachines()
+    .refresh(candyMachine)
+    .run();
+  t.deepEquals(updatedCandyMachine.items, [
+    {
+      index: 0,
+      minted: false,
+      name: 'Degen #1',
+      uri: 'https://example.com/degen/1.json',
+    },
+    {
+      index: 1,
+      minted: false,
+      name: 'Degen #2',
+      uri: 'https://example.com/degen/2.json',
+    },
+  ]);
+});
+
+test('[candyMachineModule] it cannot add items to a candy machine with hidden settings', async (t) => {
+  // Given a Candy Machine with hidden settings.
+  const mx = await metaplex();
+  const candyMachine = await createCandyMachine(mx, {
+    itemsAvailable: toBigNumber(10),
+    itemSettings: {
+      type: 'hidden',
+      name: 'Degen #$ID+1$',
+      uri: 'https://example.com/degen/$ID+1$.json',
+      hash: create32BitsHash('some-file'),
+    },
+  });
+
+  // When we try to add items to the Candy Machine.
+  const promise = mx
+    .candyMachines()
+    .insertItems({
+      candyMachine,
+      items: [
+        { name: '1', uri: '1.json' },
+        { name: '2', uri: '2.json' },
+      ],
+    })
+    .run();
+
+  // Then we expect an error from the program.
+  await assertThrows(t, promise, /HiddenSettingsDoNotHaveConfigLines/);
+});
+
+test.skip('[candyMachineModule] it cannot add items that would make the candy machine exceed the maximum capacity', async (t) => {
   // Given an existing Candy Machine with a capacity of 2 items.
   const mx = await metaplex();
   const { candyMachine } = await createCandyMachine(mx, {
@@ -81,7 +158,7 @@ test('[candyMachineModule] it cannot add items that would make the candy machine
   await assertThrows(t, promise, /Candy Machine Cannot Add Amount/);
 });
 
-test('[candyMachineModule] it cannot add items once the candy machine is fully loaded', async (t) => {
+test.skip('[candyMachineModule] it cannot add items once the candy machine is fully loaded', async (t) => {
   // Given an existing Candy Machine with 2 items loaded and a capacity of 2 items.
   const mx = await metaplex();
   const { candyMachine } = await createCandyMachine(mx, {
@@ -106,7 +183,7 @@ test('[candyMachineModule] it cannot add items once the candy machine is fully l
   await assertThrows(t, promise, /Candy Machine Is Full/);
 });
 
-test('[candyMachineModule] it cannot add items if either of them have a name or URI that is too long', async (t) => {
+test.skip('[candyMachineModule] it cannot add items if either of them have a name or URI that is too long', async (t) => {
   // Given an existing Candy Machine.
   const mx = await metaplex();
   const candyMachine = await createCandyMachine(mx);
@@ -132,7 +209,7 @@ test('[candyMachineModule] it cannot add items if either of them have a name or 
   await assertThrows(t, promise, /Candy Machine Add Item Constraints Violated/);
 });
 
-test('[candyMachineModule] it can add items to a custom offset and override existing items', async (t) => {
+test.skip('[candyMachineModule] it can add items to a custom offset and override existing items', async (t) => {
   // Given an existing Candy Machine with 2 items loaded and capacity of 3 items.
   const mx = await metaplex();
   const { candyMachine } = await createCandyMachine(mx, {
