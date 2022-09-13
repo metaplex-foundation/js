1f8a7a9d434c7f33c3121dc093fae2288be7d661
Loris Leiva <loris.leiva@gmail.com>
2022-09-13 23:55:52 +0100

    wip

--- packages/js/src/plugins/candyMachineModule/operations/updateCandyMachine.ts
+++ packages/js/src/plugins/candyMachineModule/operations/updateCandyMachine.ts
@@ -533,30 +533,27 @@ const updateCandyMachineCollectionBuilder = <
     .programs()
     .get('TokenMetadataProgram', params.programs);
 
-  const instruction = createSetCollectionInstruction(
-    {
-      candyMachine: candyMachineAddress,
-      authority: authority.publicKey,
-      authorityPda,
-      payer: payer.publicKey,
-      collectionMint: currentCollectionAddress,
-      collectionMetadata: findMetadataPda(currentCollectionAddress),
-      collectionAuthorityRecord: findCollectionAuthorityRecordPda(
-        currentCollectionAddress,
-        authorityPda
-      ),
-      newCollectionUpdateAuthority: collectionUpdateAuthority.publicKey,
-      newCollectionMetadata: findMetadataPda(collectionAddress),
-      newCollectionMint: collectionAddress,
-      newCollectionMasterEdition: findMasterEditionV2Pda(collectionAddress),
-      newCollectionAuthorityRecord: findCollectionAuthorityRecordPda(
-        collectionAddress,
-        authorityPda
-      ),
-      tokenMetadataProgram: tokenMetadataProgram.address,
-    },
-    { authorityPdaBump: authorityPda.bump }
-  );
+  const instruction = createSetCollectionInstruction({
+    candyMachine: candyMachineAddress,
+    authority: authority.publicKey,
+    authorityPda,
+    payer: payer.publicKey,
+    collectionMint: currentCollectionAddress,
+    collectionMetadata: findMetadataPda(currentCollectionAddress),
+    collectionAuthorityRecord: findCollectionAuthorityRecordPda(
+      currentCollectionAddress,
+      authorityPda
+    ),
+    newCollectionUpdateAuthority: collectionUpdateAuthority.publicKey,
+    newCollectionMetadata: findMetadataPda(collectionAddress),
+    newCollectionMint: collectionAddress,
+    newCollectionMasterEdition: findMasterEditionV2Pda(collectionAddress),
+    newCollectionAuthorityRecord: findCollectionAuthorityRecordPda(
+      collectionAddress,
+      authorityPda
+    ),
+    tokenMetadataProgram: tokenMetadataProgram.address,
+  });
 
   // TODO(loris): remove quick-fix when protocol is ready. The test should pass without it.
   instruction.keys[7].isWritable = true;
