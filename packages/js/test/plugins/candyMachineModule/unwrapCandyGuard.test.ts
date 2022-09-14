5272f8a3fe9a60f2f8f11af549b21361457d5f33
Loris Leiva <loris.leiva@gmail.com>
2022-09-14 14:23:29 +0100

    wip

---
+++ packages/js/test/plugins/candyMachineModule/unwrapCandyGuard.test.ts
@@ -0,0 +1,43 @@
+import { CandyMachine } from '@/index';
+import { Keypair } from '@solana/web3.js';
+import spok, { Specifications } from 'spok';
+import test from 'tape';
+import { killStuckProcess, metaplex, spokSamePubkey } from '../../helpers';
+import { createCandyMachine } from './helpers';
+
+killStuckProcess();
+
+test('[candyMachineModule] it can unwrap a Candy Machine from Candy Guard', async (t) => {
+  // Given a Candy Machine with a Candy Guard.
+  const mx = await metaplex();
+  const candyMachineAuthority = Keypair.generate();
+  const candyMachine = await createCandyMachine(mx, {
+    authority: candyMachineAuthority,
+  });
+  t.notEqual(candyMachine.candyGuard, null);
+
+  // When we unwrap the Candy Machine from its Candy Guard.
+  await mx
+    .candyMachines()
+    .unwrapCandyGuard({
+      candyMachine: candyMachine.address,
+      candyMachineAuthority,
+      candyGuard: candyMachine.address,
+      candyGuardAuthority: candyMachineAuthority,
+    })
+    .run();
+
+  // Then the Candy Machine's data was updated accordingly.
+  const updatedCandyMachine = await mx
+    .candyMachines()
+    .refresh(candyMachine)
+    .run();
+  spok(t, updatedCandyMachine, {
+    $topic: 'Updated Candy Machine',
+    model: 'candyMachine',
+    address: spokSamePubkey(candyMachine.address),
+    authorityAddress: spokSamePubkey(candyMachineAuthority.publicKey),
+    mintAuthorityAddress: spokSamePubkey(candyMachineAuthority.publicKey),
+    candyGuard: null,
+  } as unknown as Specifications<CandyMachine>);
+});
