35dacbcdf767533b9513e22a6e2404e2431cef5a
Loris Leiva <loris.leiva@gmail.com>
2022-09-22 14:28:18 +0100

    wip

--- packages/js/test/plugins/candyMachineModule/mintFromCandyMachine.test.ts
+++ packages/js/test/plugins/candyMachineModule/mintFromCandyMachine.test.ts
@@ -1,8 +1,17 @@
-import { CandyMachine, Nft, toBigNumber } from '@/index';
+import {
+  CandyMachine,
+  Metaplex,
+  Nft,
+  NftWithToken,
+  now,
+  sol,
+  toBigNumber,
+  toDateTime,
+} from '@/index';
 import { TokenStandard } from '@metaplex-foundation/mpl-token-metadata';
-import { Keypair } from '@solana/web3.js';
+import { Keypair, PublicKey } from '@solana/web3.js';
 import spok, { Specifications } from 'spok';
-import test from 'tape';
+import test, { Test } from 'tape';
 import {
   assertThrows,
   killStuckProcess,
@@ -14,7 +23,7 @@ import { createCandyMachine } from './helpers';
 
 killStuckProcess();
 
-test('[candyMachineModule] it can mint from a Candy Machine directly as the mint authority', async (t) => {
+test.only('[candyMachineModule] it can mint from a Candy Machine directly as the mint authority', async (t) => {
   // Given a loaded Candy Machine with a mint authority.
   const mx = await metaplex();
   const candyMachineAuthority = Keypair.generate();
@@ -122,7 +131,7 @@ test('[candyMachineModule] it cannot mint from a Candy Machine directly if not t
   await assertThrows(t, promise, /A has_one constraint was violated/);
 });
 
-test.only('[candyMachineModule] it can mint from a Candy Guard with no guards', async (t) => {
+test('[candyMachineModule] it can mint from a Candy Guard with no guards', async (t) => {
   // Given a loaded Candy Machine with a Candy Guard.
   const mx = await metaplex();
   const { candyMachine, collection } = await createCandyMachine(mx, {
@@ -198,7 +207,50 @@ test.only('[candyMachineModule] it can mint from a Candy Guard with no guards', 
 });
 
 test.skip('[candyMachineModule] it can mint from a Candy Guard with some guards', async (t) => {
-  //
+  // Given a loaded Candy Machine with some guards.
+  const mx = await metaplex();
+  const treasury = Keypair.generate();
+  const { candyMachine, collection } = await createCandyMachine(mx, {
+    itemsAvailable: toBigNumber(2),
+    items: [
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+      { name: 'Degen #2', uri: 'https://example.com/degen/2' },
+    ],
+    guards: {
+      liveDate: {
+        date: toDateTime(now().subn(3600 * 24)), // Yesterday.
+      },
+      lamports: {
+        amount: sol(1),
+        destination: treasury.publicKey,
+      },
+    },
+  });
+
+  // When we mint an NFT from this candy machine.
+  const { nft } = await mx
+    .candyMachines()
+    .mint({
+      candyMachine,
+      collectionUpdateAuthority: collection.updateAuthority.publicKey,
+    })
+    .run();
+
+  // Then an NFT was created with the right data.
+  spok(t, nft, {
+    $topic: 'Minted NFT',
+    model: 'nft',
+    name: 'Degen #1',
+  } as Specifications<Nft>);
+
+  // And the Candy Machine data was updated.
+  const updatedCandyMachine = await mx
+    .candyMachines()
+    .refresh(candyMachine)
+    .run();
+  t.equal(updatedCandyMachine.itemsMinted.toNumber(), 1);
+  t.true(updatedCandyMachine.items[0].minted, 'First item was minted');
+  t.false(updatedCandyMachine.items[1].minted, 'Second item was not minted');
 });
 
 test.skip("[candyMachineModule] it throws a bot tax error if minting succeeded but we couldn't find the mint NFT", async (t) => {
@@ -220,3 +272,74 @@ test.skip('[candyMachineModule] it cannot mint using a labelled group if the Can
 test.skip('[candyMachineModule] it cannot mint from a Candy Guard with groups if the provided group label does not exist', async (t) => {
   //
 });
+
+const assertMintingWasSuccessful = async (
+  t: Test,
+  metaplex: Metaplex,
+  input: {
+    candyMachine: CandyMachine;
+    collectionUpdateAuthority: PublicKey;
+    nft: NftWithToken;
+    owner: PublicKey;
+    mintedIndex?: number;
+  }
+) => {
+  const candyMachine = input.candyMachine;
+  const mintedIndex =
+    input.mintedIndex ?? candyMachine.itemsMinted.toNumber() - 1;
+  const expectedItemMinted = candyMachine.items[mintedIndex];
+
+  // Then an NFT was created with the right data.
+  spok(t, input.nft, {
+    $topic: 'Minted NFT',
+    model: 'nft',
+    name: expectedItemMinted.name,
+    uri: expectedItemMinted.uri,
+    symbol: candyMachine.symbol,
+    sellerFeeBasisPoints: candyMachine.sellerFeeBasisPoints,
+    tokenStandard: TokenStandard.NonFungible,
+    isMutable: candyMachine.isMutable,
+    primarySaleHappened: true,
+    updateAuthorityAddress: spokSamePubkey(input.collectionUpdateAuthority),
+    creators: [
+      {
+        address: spokSamePubkey(
+          metaplex.candyMachines().pdas().authority({
+            candyMachine: candyMachine.address,
+          })
+        ),
+        verified: true,
+        share: 0,
+      },
+      ...candyMachine.creators.map((creator) => ({
+        ...creator,
+        address: spokSamePubkey(creator.address),
+        verified: false,
+      })),
+    ],
+    edition: {
+      model: 'nftEdition',
+      isOriginal: true,
+      supply: spokSameBignum(toBigNumber(0)),
+      maxSupply: spokSameBignum(candyMachine.maxEditionSupply),
+    },
+  } as Specifications<Nft>);
+
+  // And the Candy Machine data was updated.
+  const expectedMinted = candyMachine.itemsMinted.addn(1);
+  const expectedRemaining = candyMachine.itemsAvailable.sub(expectedMinted);
+  const updatedCandyMachine = await metaplex
+    .candyMachines()
+    .refresh(candyMachine)
+    .run();
+  spok(t, updatedCandyMachine, {
+    $topic: 'Update Candy Machine',
+    itemsAvailable: spokSameBignum(candyMachine.itemsAvailable),
+    itemsMinted: spokSameBignum(expectedMinted),
+    itemsRemaining: spokSameBignum(expectedRemaining),
+  } as Specifications<CandyMachine>);
+  t.true(
+    updatedCandyMachine.items[mintedIndex].minted,
+    'Item was marked as minted'
+  );
+};
