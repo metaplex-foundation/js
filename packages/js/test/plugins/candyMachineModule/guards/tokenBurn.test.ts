e29700639e1eed2ee226b6af8d54112a38bc2dbe
Loris Leiva <loris.leiva@gmail.com>
2022-09-26 23:59:37 +0100

    wip

--- packages/js/test/plugins/candyMachineModule/guards/tokenBurn.test.ts
+++ packages/js/test/plugins/candyMachineModule/guards/tokenBurn.test.ts
@@ -1,4 +1,5 @@
-import { isEqualToAmount, sol, toBigNumber } from '@/index';
+import { isEqualToAmount, sol, toBigNumber, token } from '@/index';
+import { Keypair } from '@solana/web3.js';
 import test from 'tape';
 import {
   assertThrows,
@@ -10,19 +11,32 @@ import { assertMintingWasSuccessful, createCandyMachine } from '../helpers';
 
 killStuckProcess();
 
-test.skip('[candyMachineModule] tokenBurn guard: it allows TODO', async (t) => {
-  // Given a loaded Candy Machine with TODO.
+test.only('[candyMachineModule] tokenBurn guard: it burns a specific token to allow minting', async (t) => {
+  // Given a payer with one token.
   const mx = await metaplex();
+  const payer = await createWallet(mx, 10);
+  const { token: payerTokens } = await mx
+    .tokens()
+    .createTokenWithMint({
+      mintAuthority: Keypair.generate(),
+      owner: payer.publicKey,
+      initialSupply: token(1),
+    })
+    .run();
+
+  // And a loaded Candy Machine with the tokenBurn guard.
   const { candyMachine, collection } = await createCandyMachine(mx, {
     itemsAvailable: toBigNumber(1),
     items: [{ name: 'Degen #1', uri: 'https://example.com/degen/1' }],
     guards: {
-      TODO: {},
+      tokenBurn: {
+        mint: payerTokens.mint.address,
+        amount: token(1),
+      },
     },
   });
 
   // When we mint from it.
-  const payer = await createWallet(mx, 10);
   const { nft } = await mx
     .candyMachines()
     .mint({
@@ -39,9 +53,20 @@ test.skip('[candyMachineModule] tokenBurn guard: it allows TODO', async (t) => {
     nft,
     owner: payer.publicKey,
   });
+
+  // And the payer's token was burned.
+  // TODO
 });
 
-test.skip('[candyMachineModule] tokenBurn guard: it forbids TODO', async (t) => {
+test.skip('[candyMachineModule] tokenBurn guard: it may burn multiple tokens from a specific mint', async (t) => {
+  //
+});
+
+test.skip('[candyMachineModule] tokenBurn guard: it may explicitely provide the token account and burn authority', async (t) => {
+  //
+});
+
+test.skip('[candyMachineModule] tokenBurn guard: it fails to mint if there are not enough tokens to burn', async (t) => {
   //
 });
 
