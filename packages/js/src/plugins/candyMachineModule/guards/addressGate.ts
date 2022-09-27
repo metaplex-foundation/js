32139f21eb1ff2be9b5c399322ed4cf37e86a636
Loris Leiva <loris.leiva@gmail.com>
2022-09-27 14:25:20 +0100

    wip

--- packages/js/src/plugins/candyMachineModule/guards/addressGate.ts
+++ packages/js/src/plugins/candyMachineModule/guards/addressGate.ts
@@ -5,7 +5,11 @@ import {
 } from '@metaplex-foundation/mpl-candy-guard';
 import { CandyGuardManifest } from './core';
 
-/** TODO */
+/**
+ * The settings for the addressGate guard that should
+ * be provided when creating and/or updating
+ * a Candy Machine or a Candy Guard directly.
+ */
 export type AddressGateGuardSettings = AddressGate;
 
 /** @internal */
--- packages/js/src/plugins/candyMachineModule/guards/gatekeeper.ts
+++ packages/js/src/plugins/candyMachineModule/guards/gatekeeper.ts
@@ -11,14 +11,50 @@ import {
 import { Buffer } from 'buffer';
 import { CandyGuardManifest, CandyGuardsMintRemainingAccount } from './core';
 
-/** TODO */
+/**
+ * The settings for the gatekeeper guard that should
+ * be provided when creating and/or updating
+ * a Candy Machine or a Candy Guard directly.
+ */
 export type GatekeeperGuardSettings = {
+  /**
+   * The public key of the Gatekeeper Network that will
+   * be used to check the validity of the minting wallet.
+   *
+   * For instance, you may use the "Civic Captcha Pass" Network,
+   * which ensures the minting wallet has passed a captcha, by using
+   * the following address: `ignREusXmGrscGNUesoU9mxfds9AiYTezUKex2PsZV6`.
+   */
   network: PublicKey;
+
+  /**
+   * Whether we should mark the Gateway Token of the minting wallet
+   * as expired after the NFT has been minting.
+   *
+   * When set to `true`, they will need to go through the Gatekeeper
+   * Network again in order to mint another NFT.
+   *
+   * When set to `false`, they will be able to mint another NFT
+   * until the Gateway Token expires naturally.
+   */
   expireOnUse: boolean;
 };
 
-/** TODO */
+/**
+ * The settings for the gatekeeper guard that could
+ * be provided when minting from the Candy Machine.
+ */
 export type GatekeeperGuardMintSettings = {
+  /**
+   * The Gateway Token PDA derived from the payer
+   * and the Gatekeeper Network which is used to
+   * verify the payer's eligibility to mint.
+   *
+   * @defaultValue
+   * Computes the Gateway Token PDA using the payer's and the
+   * Gatekeeper Network's public keys as well as the default
+   * `seed` value which is `[0, 0, 0, 0, 0, 0, 0, 0]`.
+   */
   tokenAccount?: PublicKey;
 };
 
