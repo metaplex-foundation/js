eec71424f883cab7502bed4c2cfef5022c9b2ea9
Loris Leiva <loris.leiva@gmail.com>
2022-10-04 10:27:05 +0100

    chore: remove unused export

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -3,6 +3,3 @@ export * from './plugins';
 export * from './types';
 export * from './utils';
 export * from './Metaplex';
-
-// Need to be referenced here otherwise rollup will not include the file.
-export * from './accountProviders';
