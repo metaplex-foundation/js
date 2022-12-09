db310b5cad68e3861c26d1327f95cfc9ccbcd0b7
Loris Leiva <loris.leiva@gmail.com>
2022-12-09 12:23:53 +0000

    Simplify MetaplexError and ProgramErrors

--- packages/js/src/errors/MetaplexError.ts
+++ packages/js/src/errors/MetaplexError.ts
@@ -1,68 +1,20 @@
-export type MetaplexErrorSource =
-  | 'sdk'
-  | 'network'
-  | 'rpc'
-  | 'plugin'
-  | 'program';
-
-export type MetaplexErrorInput = {
-  key: string;
-  title: string;
-  problem: string;
-  solution: string;
-  source: MetaplexErrorSource;
-  sourceDetails?: string;
-  options?: MetaplexErrorOptions;
-};
-
-export type MetaplexErrorInputWithoutSource = Omit<
-  MetaplexErrorInput,
-  'source' | 'sourceDetails'
->;
-
-export type MetaplexErrorOptions = {
-  problem?: string;
-  problemPrefix?: string;
-  problemSuffix?: string;
-  solution?: string;
-  solutionPrefix?: string;
-  solutionSuffix?: string;
-  cause?: Error;
-  logs?: string[];
-};
-
 /** @group Errors */
 export class MetaplexError extends Error {
-  readonly name: 'MetaplexError' = 'MetaplexError';
-  readonly key: string;
-  readonly title: string;
-  readonly problem: string;
-  readonly solution: string;
+  readonly name: string = 'MetaplexError';
   readonly source: MetaplexErrorSource;
   readonly sourceDetails?: string;
   readonly cause?: Error;
-  readonly logs?: string[];
 
-  constructor(input: MetaplexErrorInput) {
-    super(input.problem);
-    this.key = `metaplex.errors.${input.key}`;
-    this.title = input.title;
-    this.problem = overrideWithOptions(
-      input.problem,
-      input.options?.problem,
-      input.options?.problemPrefix,
-      input.options?.problemSuffix
-    );
-    this.solution = overrideWithOptions(
-      input.solution,
-      input.options?.solution,
-      input.options?.solutionPrefix,
-      input.options?.solutionSuffix
-    );
-    this.source = input.source;
-    this.sourceDetails = input.sourceDetails;
-    this.cause = input.options?.cause;
-    this.logs = input.options?.logs;
+  constructor(
+    message: string,
+    source: MetaplexErrorSource,
+    sourceDetails?: string,
+    cause?: Error
+  ) {
+    super(message);
+    this.source = source;
+    this.sourceDetails = sourceDetails;
+    this.cause = cause;
     this.message = this.toString(false);
   }
 
@@ -82,30 +34,20 @@ export class MetaplexError extends Error {
   }
 
   toString(withName = true) {
-    const logs =
-      this.logs != null ? `\n\n[ Logs: ${this.logs.join(' |$> ')} ]` : '';
-    const causedBy = this.cause ? `\n\nCaused By: ${this.cause}` : '';
-
     return (
       (withName ? `[${this.name}] ` : '') +
-      `${this.title}` +
-      `\n>> Source: ${this.getFullSource()}` +
-      `\n>> Problem: ${this.problem}` +
-      `\n>> Solution: ${this.solution}` +
-      causedBy +
-      logs +
+      `${this.message} ` +
+      `\n\nSource: ${this.getFullSource()}` +
+      (this.cause ? `\n\nCaused By: ${this.cause}` : '') +
       '\n'
     );
   }
 }
 
-const overrideWithOptions = (
-  defaultText: string,
-  override?: string,
-  prefix?: string,
-  suffix?: string
-): string => {
-  return [prefix, override ?? defaultText, suffix]
-    .filter((text): text is string => !!text)
-    .join(' ');
-};
+/** @group Errors */
+export type MetaplexErrorSource =
+  | 'sdk'
+  | 'network'
+  | 'rpc'
+  | 'plugin'
+  | 'program';
--- packages/js/src/errors/ProgramError.ts
+++ packages/js/src/errors/ProgramError.ts
@@ -1,79 +1,56 @@
-import {
-  MetaplexError,
-  MetaplexErrorInputWithoutSource,
-  MetaplexErrorOptions,
-} from './MetaplexError';
+import { MetaplexError } from './MetaplexError';
 import { Program } from '@/types';
+
+/** @group Errors */
+export class ProgramError extends MetaplexError {
+  readonly name: string = 'ProgramError';
+  readonly program: Program;
+  readonly logs?: string[];
+
+  constructor(
+    message: string,
+    program: Program,
+    cause?: Error,
+    logs?: string[]
+  ) {
+    super(
+      message,
+      'program',
+      `${program.name} [${program.address.toString()}]`,
+      cause
+    );
+    this.program = program;
+    this.logs = logs;
+  }
+}
 
 type UnderlyingProgramError = Error & { code?: number; logs?: string[] };
 
 /** @group Errors */
-export class ProgramError extends MetaplexError {
-  public program: Program;
-
-  constructor(program: Program, input: MetaplexErrorInputWithoutSource) {
-    super({
-      ...input,
-      key: `program.${input.key}`,
-      title: `${program.name} > ${input.title}`,
-      source: 'program',
-      sourceDetails: `${program.name} [${program.address.toBase58()}]`,
-    });
-    this.program = program;
-  }
-}
-
-/** @group Errors */
 export class ParsedProgramError extends ProgramError {
-  constructor(
-    program: Program,
-    cause: UnderlyingProgramError,
-    options?: Omit<MetaplexErrorOptions, 'cause'>
-  ) {
+  readonly name: string = 'ParsedProgramError';
+  constructor(program: Program, cause: UnderlyingProgramError, logs: string[]) {
     const ofCode = cause.code ? ` of code [${cause.code}]` : '';
-    super(program, {
-      key: 'parsed_program_error',
-      title: cause.message,
-      problem:
-        `The program [${program.name}] ` +
-        `at address [${program.address.toBase58()}] ` +
-        `raised an error${ofCode} ` +
-        `that translates to "${cause.message}".`,
-      solution: 'Check the error message provided by the program.',
-      options: {
-        ...options,
-        cause,
-      },
-    });
+    const message =
+      `The program [${program.name}] ` +
+      `at address [${program.address.toString()}] ` +
+      `raised an error${ofCode} ` +
+      `that translates to "${cause.message}".`;
+    super(message, program, cause, logs);
   }
 }
 
 /** @group Errors */
 export class UnknownProgramError extends ProgramError {
-  constructor(
-    program: Program,
-    cause: UnderlyingProgramError,
-    options?: Omit<MetaplexErrorOptions, 'cause' | 'logs'>
-  ) {
+  readonly name: string = 'UnknownProgramError';
+  constructor(program: Program, cause: UnderlyingProgramError) {
     const ofCode = cause.code ? ` of code [${cause.code}]` : '';
-    super(program, {
-      key: 'unknown_program_error',
-      title: 'Unknown Program Error',
-      problem:
-        `The program [${
-          program.name
-        }] at address [${program.address.toBase58()}] ` +
-        `raised an error${ofCode} that is not recognized by the programs registered by the SDK.`,
-      solution:
-        'Unfortunately, you will need to check the unparsed ' +
-        'error below to investigate what went wrong. ' +
-        'To get more helpful error messages, ensure the program that failed is ' +
-        'registered by the SDK and provides an "errorResolver" method.',
-      options: {
-        ...options,
-        logs: cause.logs,
-        cause,
-      },
-    });
+    const message =
+      `The program [${program.name}] ` +
+      `at address [${program.address.toString()}] ` +
+      `raised an error${ofCode} ` +
+      `that is not recognized by the programs registered on the SDK. ` +
+      `Please check the underlying program error below for more details.`;
+    super(message, program, cause, cause.logs);
   }
 }
