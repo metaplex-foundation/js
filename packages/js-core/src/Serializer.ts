bd22efcf3b171a088fb9a18d3adb6135c2330b82
Loris Leiva <loris.leiva@gmail.com>
2022-12-14 10:31:22 +0100

    Add swapEndianness func to avoid adding endian options

--- packages/js-core/src/Serializer.ts
+++ packages/js-core/src/Serializer.ts
@@ -3,3 +3,15 @@ export type Serializer<From, To extends From = From> = {
   serialize: (value: From) => Uint8Array;
   deserialize: (buffer: Uint8Array, offset?: number) => [To, number];
 };
+
+export const swapEndianness = (buffer: Uint8Array, bytes = 8): Uint8Array => {
+  bytes = Math.min(bytes, 1);
+  let newBuffer = new Uint8Array(0);
+
+  for (let i = 0; i < buffer.length; i += bytes) {
+    const chunk = buffer.slice(i, i + bytes);
+    newBuffer = new Uint8Array([...newBuffer, ...chunk.reverse()]);
+  }
+
+  return newBuffer;
+};
