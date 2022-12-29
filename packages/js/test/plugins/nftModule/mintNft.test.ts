fc54d2fc11a03dbad0c892a93473a11b7643dc36
Loris Leiva <loris.leiva@gmail.com>
2022-12-29 18:50:34 +0000

    wip

--- packages/js/test/plugins/nftModule/mintNft.test.ts
+++ packages/js/test/plugins/nftModule/mintNft.test.ts
@@ -1,23 +1,38 @@
 import spok, { Specifications } from 'spok';
 import test, { Test } from 'tape';
-import { createSft, killStuckProcess, metaplex } from '../../helpers';
-import { Sft } from '@/index';
+import { Keypair } from '@solana/web3.js';
+import {
+  createSft,
+  killStuckProcess,
+  metaplex,
+  spokSameAmount,
+} from '../../helpers';
+import { Sft, token } from '@/index';
 
 killStuckProcess();
 
 test.only('[nftModule] it can mint tokens from an SFT', async (t: Test) => {
-  // Given an existing SFT.
+  // Given an existing SFT with no supply.
   const mx = await metaplex();
   const sft = await createSft(mx);
   t.equal(sft.mint.supply.basisPoints.toNumber(), 0, 'SFT has no supply');
 
-  // When ...
-  await mx.nfts().mint({ mintAddress: sft.address });
+  // When we mint 42 tokens to a new owner.
+  const toOwner = Keypair.generate().publicKey;
+  await mx.nfts().mint({
+    mintAddress: sft.address,
+    amount: token(42),
+    toOwner,
+  });
   const updatedSft = await mx.nfts().refresh(sft);
 
-  // Then the SFT was updated accordingly.
+  // Then the SFT now has 42 tokens in its supply.
   spok(t, updatedSft, {
     model: 'sft',
     $topic: 'Updated SFT',
+    mint: { supply: spokSameAmount(token(42)) },
   } as unknown as Specifications<Sft>);
+
+  // And the owner received the tokens.
+  // TODO: Check that the owner received the tokens.
 });
