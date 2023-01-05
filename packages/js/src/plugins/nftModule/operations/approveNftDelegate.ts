1b05180842502466458e9ae7dc0e98e14f4f6e52
Loris Leiva <loris.leiva@gmail.com>
2023-01-05 10:19:14 +0000

    wip

--- packages/js/src/plugins/nftModule/operations/approveNftDelegate.ts
+++ packages/js/src/plugins/nftModule/operations/approveNftDelegate.ts
@@ -1,11 +1,22 @@
-import { createMintInstruction } from '@metaplex-foundation/mpl-token-metadata';
-import { PublicKey } from '@solana/web3.js';
+import {
+  createDelegateInstruction,
+  DelegateArgs,
+} from '@metaplex-foundation/mpl-token-metadata';
+import { PublicKey, SYSVAR_INSTRUCTIONS_PUBKEY } from '@solana/web3.js';
 import { SendAndConfirmTransactionResponse } from '../../rpcModule';
+import { isNonFungible, Sft } from '../models';
+import {
+  parseTokenMetadataAuthorization,
+  TokenMetadataAuthorityHolder,
+  TokenMetadataAuthorityMetadata,
+  TokenMetadataAuthorizationDetails,
+} from '../Authorization';
 import { Metaplex } from '@/Metaplex';
 import {
   Operation,
   OperationHandler,
   OperationScope,
+  Signer,
   useOperation,
 } from '@/types';
 import { TransactionBuilder, TransactionBuilderOptions } from '@/utils';
@@ -44,8 +55,47 @@ export type ApproveNftDelegateOperation = Operation<
  * @category Inputs
  */
 export type ApproveNftDelegateInput = {
-  /** The address of the mint account. */
-  mintAddress: PublicKey;
+  /**
+   * The NFT or SFT to mint from.
+   * We only need its address and token standard.
+   */
+  nftOrSft: Pick<Sft, 'address' | 'tokenStandard'>;
+
+  /**
+   * An authority allowed to approve a new delegate authority.
+   *
+   * Note that Delegate authorities are not supported for this
+   * instruction as delegates cannot approve other delegates.
+   *
+   * If a `Signer` is provided directly,
+   * it will be used as the update authority.
+   *
+   * @see {@link TokenMetadataAuthority}
+   * @defaultValue `metaplex.identity()`
+   */
+  authority?:
+    | Signer
+    | TokenMetadataAuthorityMetadata
+    | TokenMetadataAuthorityHolder;
+
+  /**
+   * The authorization rules and data to use for the mint.
+   *
+   * @see {@link TokenMetadataAuthorizationDetails}
+   * @defaultValue Defaults to not using auth rules.
+   */
+  authorizationDetails?: TokenMetadataAuthorizationDetails;
+
+  /**
+   * The address of the new delegate authority to approve.
+   */
+  delegate: PublicKey;
+
+  /**
+   * The type of delegate to approve, including any
+   * additional data it might need.
+   */
+  delegateType: DelegateArgs;
 };
 
 /**
@@ -111,15 +161,36 @@ export const approveNftDelegateBuilder = (
   options: TransactionBuilderOptions = {}
 ): TransactionBuilder => {
   const { programs, payer = metaplex.rpc().getDefaultFeePayer() } = options;
-  const { mintAddress } = params;
+  const {
+    nftOrSft,
+    authority = metaplex.identity(),
+    authorizationDetails,
+    delegate,
+    delegateType,
+  } = params;
 
   // Programs.
   const tokenMetadataProgram = metaplex.programs().getTokenMetadata(programs);
+  const tokenProgram = metaplex.programs().getToken(programs);
+  const systemProgram = metaplex.programs().getSystem(programs);
 
   // PDAs.
   const metadata = metaplex.nfts().pdas().metadata({
-    mint: mintAddress,
+    mint: nftOrSft.address,
     programs,
+  });
+  const masterEdition = metaplex.nfts().pdas().masterEdition({
+    mint: nftOrSft.address,
+    programs,
+  });
+
+  // Auth.
+  const auth = parseTokenMetadataAuthorization({
+    authority:
+      '__kind' in authority
+        ? authority
+        : { __kind: 'metadata', updateAuthority: authority },
+    authorizationDetails,
   });
 
   return (
@@ -128,12 +199,24 @@ export const approveNftDelegateBuilder = (
 
       // Update the metadata account.
       .add({
-        instruction: createMintInstruction(
-          { metadata } as any, // TODO
-          {} as any, // TODO
+        instruction: createDelegateInstruction(
+          {
+            ...auth.accounts,
+            delegateRecord: TODO,
+            delegate,
+            metadata,
+            masterEdition: isNonFungible(nftOrSft) ? masterEdition : undefined,
+            mint: nftOrSft.address,
+            payer: payer.publicKey,
+            systemProgram: systemProgram.address,
+            sysvarInstructions: SYSVAR_INSTRUCTIONS_PUBKEY,
+            splTokenProgram: tokenProgram.address,
+            // authorizationRulesProgram,
+          },
+          { delegateArgs: delegateType },
           tokenMetadataProgram.address
         ),
-        signers: [],
+        signers: [payer],
         key: params.instructionKey ?? 'approveNftDelegate',
       })
   );
