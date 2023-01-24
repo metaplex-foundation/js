092e6445bc9c6271828c264aee08eeb27c69d44d
Loris Leiva <loris.leiva@gmail.com>
2023-01-24 15:16:38 +0000

    wip

--- packages/js/test/plugins/nftModule/lockNft.test.ts
+++ packages/js/test/plugins/nftModule/lockNft.test.ts
@@ -1,40 +1,62 @@
-import spok, { Specifications } from 'spok';
-import test, { Test } from 'tape';
-import { TokenStandard } from '@metaplex-foundation/mpl-token-metadata';
+import {
+  TokenRecord,
+  TokenStandard,
+  TokenState,
+} from '@metaplex-foundation/mpl-token-metadata';
 import { Keypair } from '@solana/web3.js';
+import test, { Test } from 'tape';
 import { createNft, killStuckProcess, metaplex } from '../../helpers';
-import { Nft } from '@/index';
 
 killStuckProcess();
 
 test('[nftModule] utility delegates can lock programmable NFTs', async (t: Test) => {
   // Given an existing PNFT.
   const mx = await metaplex();
-  const owner = Keypair.generate();
+  const nftOwner = Keypair.generate();
   const nft = await createNft(mx, {
     tokenStandard: TokenStandard.ProgrammableNonFungible,
-    tokenOwner: owner.publicKey,
+    tokenOwner: nftOwner.publicKey,
   });
 
   // And an approved utility delegate.
   const utilityDelegate = Keypair.generate();
   await mx.nfts().delegate({
     nftOrSft: nft,
+    authority: nftOwner,
     delegate: {
       type: 'UtilityV1',
       delegate: utilityDelegate.publicKey,
-      owner: owner.publicKey,
+      owner: nftOwner.publicKey,
       data: { amount: 1 },
     },
   });
 
-  // When ...
-  // Do something...
+  // And the PNFT is unlocked.
+  const tokenRecord = mx.nfts().pdas().tokenRecord({
+    mint: nft.address,
+    token: nft.token.address,
+  });
+  let tokenRecordAccount = await TokenRecord.fromAccountAddress(
+    mx.connection,
+    tokenRecord
+  );
+  t.equal(tokenRecordAccount.state, TokenState.Unlocked);
 
-  // Then the NFT was updated accordingly.
-  const updatedNft = await mx.nfts().refresh(nft);
-  spok(t, updatedNft, {
-    model: 'nft',
-    $topic: 'Updated NFT',
-  } as unknown as Specifications<Nft>);
+  // When the utility delegate locks the PNFT.
+  await mx.nfts().lock({
+    nftOrSft: nft,
+    authority: {
+      __kind: 'tokenDelegate',
+      type: 'UtilityV1',
+      delegate: utilityDelegate,
+      owner: nftOwner.publicKey,
+    },
+  });
+
+  // Then the PNFT has been locked.
+  tokenRecordAccount = await TokenRecord.fromAccountAddress(
+    mx.connection,
+    tokenRecord
+  );
+  t.equal(tokenRecordAccount.state, TokenState.Locked);
 });
