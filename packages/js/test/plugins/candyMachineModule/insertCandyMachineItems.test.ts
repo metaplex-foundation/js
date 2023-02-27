0953ed379a230d4d4f6aadfade1674a20d2695db
Loris Leiva <loris.leiva@gmail.com>
2023-02-27 14:42:40 +0000

    Allow updating items of fully loaded CMs

--- packages/js/test/plugins/candyMachineModule/insertCandyMachineItems.test.ts
+++ packages/js/test/plugins/candyMachineModule/insertCandyMachineItems.test.ts
@@ -261,3 +261,42 @@ test('[candyMachineModule] it can add items to a custom offset and override exis
     },
   ]);
 });
+
+test('[candyMachineModule] it can override all items of a candy machine', async (t) => {
+  // Given an fully loaded Candy Machine with 2 items.
+  const mx = await metaplex();
+  const { candyMachine } = await createCandyMachine(mx, {
+    itemsAvailable: toBigNumber(2),
+    items: [
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+      { name: 'Degen #2', uri: 'https://example.com/degen/2' },
+    ],
+  });
+
+  // When we add 2 new items to the Candy Machine at index 0.
+  await mx.candyMachines().insertItems({
+    candyMachine,
+    index: 0,
+    items: [
+      { name: 'Degen #3', uri: 'https://example.com/degen/3' },
+      { name: 'Degen #4', uri: 'https://example.com/degen/4' },
+    ],
+  });
+
+  // Then all items have been overriden.
+  const updatedCandyMachine = await mx.candyMachines().refresh(candyMachine);
+  t.deepEquals(updatedCandyMachine.items, [
+    {
+      index: 0,
+      minted: false,
+      name: 'Degen #3',
+      uri: 'https://example.com/degen/3',
+    },
+    {
+      index: 1,
+      minted: false,
+      name: 'Degen #4',
+      uri: 'https://example.com/degen/4',
+    },
+  ]);
+});
