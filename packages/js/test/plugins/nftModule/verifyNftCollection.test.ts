72245563a5ac53e36d95e4cfb244386b6b69fe31
Loris Leiva <loris.leiva@gmail.com>
2023-05-25 18:19:41 +0100

    Add Verify ix support to verifyNftCollection operation

--- packages/js/test/plugins/nftModule/verifyNftCollection.test.ts
+++ packages/js/test/plugins/nftModule/verifyNftCollection.test.ts
@@ -86,3 +86,55 @@ test('[nftModule] it can verify the legacy collection of an NFT item', async (t:
     },
   } as unknown as Specifications<Nft>);
 });
+
+test('[nftModule] it can verify the collection of an NFT item as a metadata delegate', async (t: Test) => {
+  // Given a Metaplex instance.
+  const mx = await metaplex();
+
+  // And an existing NFT with an unverified collection.
+  const collectionAuthority = Keypair.generate();
+  const collection = await createCollectionNft(mx, {
+    updateAuthority: collectionAuthority,
+  });
+  const nft = await createNft(mx, {
+    collection: collection.address,
+  });
+  t.true(nft.collection, 'nft has a collection');
+  t.false(nft.collection?.verified, 'nft collection is not verified');
+  await assertRefreshedCollectionHasSize(t, mx, collection, 0);
+
+  // And a metadata delegate approved by the collection's update authority.
+  const collectionDelegate = Keypair.generate();
+  await mx.nfts().delegate({
+    nftOrSft: collection,
+    authority: collectionAuthority,
+    delegate: {
+      type: 'CollectionV1',
+      delegate: collectionDelegate.publicKey,
+      updateAuthority: collection.updateAuthorityAddress,
+    },
+  });
+
+  // When the metadata delegate verifies the collection.
+  await mx.nfts().verifyCollection({
+    mintAddress: nft.address,
+    collectionMintAddress: nft.collection!.address,
+    collectionAuthority: collectionDelegate,
+    collectionUpdateAuthority: collectionAuthority.publicKey,
+    isDelegated: 'metadataDelegate',
+  });
+
+  // Then the NFT collection should be verified.
+  const updatedNft = await mx.nfts().refresh(nft);
+  spok(t, updatedNft, {
+    $topic: 'Updated Nft',
+    model: 'nft',
+    collection: {
+      address: spokSamePubkey(collection.address),
+      verified: true,
+    },
+  } as unknown as Specifications<Nft>);
+
+  // And the collection should have the updated size.
+  await assertRefreshedCollectionHasSize(t, mx, collection, 1);
+});
